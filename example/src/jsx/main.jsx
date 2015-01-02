a3df9fe8c09f29e589678e30784aad3520ffae2b
gpbl <io@gpbl.org>
2015-01-02 21:10:41 +0100

    Move jsx files to jsx folder

--- example/src/jsx/main.jsx
+++ example/src/jsx/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import reactTapEvent from 'react-tap-event-plugin';
 import Page from './Page.jsx';
 
+// enable touch-tap events
 reactTapEvent();
+
+// inject css
+require("../scss/main.scss");
 
 React.render(<Page />, document.body);
