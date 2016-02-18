6b847450d5de451f04350b74abb43e77021e6d15
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-02-18 01:22:05 +0100

    Remove unused call to reactTapEvent (close #129)

--- examples/src/main.js
+++ examples/src/main.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import reactTapEvent from "react-tap-event-plugin";
 import Examples from "./Examples";
 import "./styles/main.css";
-
-// enable touch-tap events
-reactTapEvent();
 
 ReactDOM.render(<Examples />, document.getElementById("root"));
