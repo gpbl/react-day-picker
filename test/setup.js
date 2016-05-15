c0b09ec95e38509b4eebabfd79f0f0de4c0f78e8
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-05-15 17:43:57 +0200

    Refactor tests to use enzyme

--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,10 @@
 import { jsdom } from 'jsdom';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
+import chaiEnzyme from 'chai-enzyme';
+
 chai.use(sinonChai);
+chai.use(chaiEnzyme());
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
