f529641f175a8cd2d824db32cbe030f1fe76bb22
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-04 18:15:54 +0200

    Bump v2.3.3

--- src/DayPicker.js
+++ src/DayPicker.js
@@ -13,7 +13,7 @@ import * as LocaleUtils from './LocaleUtils';
 import keys from './keys';
 import DayPickerPropTypes from './PropTypes';
 export default class DayPicker extends Component {
-  static VERSION = '2.3.2';
+  static VERSION = '2.3.3';
 
   static propTypes = {
     initialMonth: PropTypes.instanceOf(Date),
