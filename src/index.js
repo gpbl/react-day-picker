177af9882533f9713dcf03011bcd87b47748b948
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-07-31 15:51:50 +0200

    Export PropTypes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 export default from './DayPicker';
 export DateUtils from './DateUtils';
 export LocaleUtils from './LocaleUtils';
+
+import Weekday from './Weekday';
+export const WeekdayPropTypes = Weekday.PropTypes;
+
+import Navbar from './Navbar';
+export const NavbarPropTypes = Navbar.PropTypes;
+
+export PropTypes from './PropTypes';
