5e8379d9ada22fa4a0f7bde6c98f0f4387f2ad7e
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-09-18 18:37:23 +0200

    Cleanup code

--- examples/src/examples/CalendarOverlay.js
+++ examples/src/examples/CalendarOverlay.js
@@ -4,99 +4,89 @@ import moment from 'moment';
 
 import '../../../src/style.css';
 
-const containerStyles = {
-  position: 'relative'
-};
-
 const calendarContainerStyles = {
   position: 'absolute',
   right: -10,
   top: -50,
   background: 'white',
   transform: 'translate(100%, 0)',
-  boxShadow: '0 2px 5px rgba(0, 0, 0, .15)'
+  boxShadow: '0 2px 5px rgba(0, 0, 0, .15)',
 };
 
 export default class CalendarOverlay extends Component {
-  state = {
-    focused: false,
-    value: ''
-  };
-
-  // Important private property which indicates if input's blur event was caused by clicking on the calendar
-  _clickInside = false;
 
   constructor(props) {
     super(props);
-    this.onDateChange = this.onDateChange.bind(this);
-    this.onFocus = this.onFocus.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-    this.onClickInside = this.onClickInside.bind(this);
-    this.setInput = this.setInput.bind(this);
+    this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleInputFocus = this.handleInputFocus.bind(this);
+    this.handleInputBlur = this.handleInputBlur.bind(this);
+    this.handleContainerMouseDown = this.handleContainerMouseDown.bind(this);
   }
 
-  onFocus() {
+  state = {
+    focused: false,
+    value: '',
+  };
+
+  clickedInside = false;
+
+  handleInputFocus() {
     this.setState({
-      focused: true
+      focused: true,
     });
   }
 
-  onClickInside() {
-    this._clickInside = true;
+  handleContainerMouseDown() {
+    this.clickedInside = true;
 
-    // Some "magic" here. onClickInside method is called by onMouseDown event.
+    // Some "magic" here. handleContainerMouseDown method is called by onMouseDown event.
     // onBlur method is called from a queue right after onMouseDown event.
     // setTimeout adds another callback in the queue, but is called later than onBlur event
     setTimeout(() => {
-      this._clickInside = false;
-    }, 0)
+      this.clickedInside = false;
+    }, 0);
   }
 
-  onBlur() {
-    const focused = this._clickInside;
+  handleInputBlur() {
+    const focused = this.clickedInside;
 
     this.setState({
-      focused
+      focused,
     });
 
     // Force input's focus if blur event was caused by clicking on the calendar
     if (focused) {
-      this._input.focus();
+      this.input.focus();
     }
   }
 
-  onDateChange(e, day) {
+  handleDayClick(e, day) {
     this.setState({
       value: moment(day).format('DD.MM.YYYY'),
-      focused: false
+      focused: false,
     });
-
-    // Force input's blur on value set, so onFocus method can be triggered again
-    this._input.blur();
-  }
-
-
-  setInput(input) {
-    this._input = input;
+    this.input.blur();
   }
 
   render() {
-    const calendar = this.state.focused
-      &&  <div style={calendarContainerStyles}>
-            <DayPicker onDayClick={this.onDateChange} />
-          </div>;
-
     return (
-      <div style={containerStyles} onMouseDown={this.onClickInside}>
-        {calendar}
+      <div
+        style={ { position: 'relative ' } }
+        onMouseDown={ this.handleContainerMouseDown }
+      >
         <input
           type="text"
-          ref={this.setInput}
+          ref={ el => { this.input = el; } }
           placeholder="Choose Date"
-          value={this.state.value}
-          onFocus={this.onFocus}
-          onBlur={this.onBlur}
+          value={ this.state.value }
+          onFocus={ this.handleInputFocus }
+          onBlur={ this.handleInputBlur }
         />
+        { this.state.focused &&
+          <div style={ calendarContainerStyles }>
+            <DayPicker onDayClick={ this.handleDayClick } />
+          </div>
+        }
       </div>
     );
   }
