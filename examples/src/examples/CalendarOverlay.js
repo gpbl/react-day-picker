c88d4e56d6f75778271526e8f1453820e0db6466
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-09-18 19:19:08 +0200

    Add ability to change input field value

--- examples/src/examples/CalendarOverlay.js
+++ examples/src/examples/CalendarOverlay.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import DayPicker from '../../../src';
+import DayPicker, { DateUtils } from '../../../src';
 import moment from 'moment';
 
 import '../../../src/style.css';
 
-const calendarContainerStyles = {
+const overlayStyle = {
   position: 'absolute',
-  right: -10,
-  top: -50,
   background: 'white',
-  transform: 'translate(100%, 0)',
   boxShadow: '0 2px 5px rgba(0, 0, 0, .15)',
 };
 
@@ -18,73 +15,96 @@ export default class CalendarOverlay extends Component {
   constructor(props) {
     super(props);
     this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleInputFocus = this.handleInputFocus.bind(this);
     this.handleInputBlur = this.handleInputBlur.bind(this);
     this.handleContainerMouseDown = this.handleContainerMouseDown.bind(this);
   }
 
   state = {
-    focused: false,
+    showOverlay: false,
     value: '',
+    selectedDay: null,
   };
 
+  input = null;
+  daypicker = null;
   clickedInside = false;
-
-  handleInputFocus() {
-    this.setState({
-      focused: true,
-    });
-  }
 
   handleContainerMouseDown() {
     this.clickedInside = true;
-
-    // Some "magic" here. handleContainerMouseDown method is called by onMouseDown event.
-    // onBlur method is called from a queue right after onMouseDown event.
+    // The input's onBlur method is called from a queue right after onMouseDown event.
     // setTimeout adds another callback in the queue, but is called later than onBlur event
     setTimeout(() => {
       this.clickedInside = false;
     }, 0);
   }
 
+  handleInputFocus() {
+    this.setState({
+      showOverlay: true,
+    });
+  }
+
   handleInputBlur() {
-    const focused = this.clickedInside;
+    const showOverlay = this.clickedInside;
 
     this.setState({
-      focused,
+      showOverlay,
     });
 
     // Force input's focus if blur event was caused by clicking on the calendar
-    if (focused) {
+    if (showOverlay) {
       this.input.focus();
     }
   }
 
+  handleInputChange(e) {
+    const { value } = e.target;
+    const momentDay = moment(value, 'L', true);
+    if (momentDay.isValid()) {
+      this.setState({
+        selectedDay: momentDay.toDate(),
+        value,
+      }, () => {
+        this.daypicker.showMonth(this.state.selectedDay);
+      });
+    } else {
+      this.setState({ value, selectedDay: null });
+    }
+  }
+
+
   handleDayClick(e, day) {
     this.setState({
-      value: moment(day).format('DD.MM.YYYY'),
-      focused: false,
+      value: moment(day).format('L'),
+      selectedDay: day,
+      showOverlay: false,
     });
     this.input.blur();
   }
 
   render() {
     return (
-      <div
-        style={ { position: 'relative ' } }
-        onMouseDown={ this.handleContainerMouseDown }
-      >
+      <div onMouseDown={ this.handleContainerMouseDown }>
         <input
           type="text"
           ref={ el => { this.input = el; } }
-          placeholder="Choose Date"
+          placeholder="DD/MM/YYYY"
           value={ this.state.value }
+          onChange={ this.handleInputChange }
           onFocus={ this.handleInputFocus }
           onBlur={ this.handleInputBlur }
         />
-        { this.state.focused &&
-          <div style={ calendarContainerStyles }>
-            <DayPicker onDayClick={ this.handleDayClick } />
+        { this.state.showOverlay &&
+          <div style={ { position: 'relative' } }>
+            <div style={ overlayStyle }>
+              <DayPicker
+                ref={ el => { this.daypicker = el; } }
+                onDayClick={ this.handleDayClick }
+                selectedDays={ day => DateUtils.isSameDay(this.state.selectedDay, day) }
+              />
+            </div>
           </div>
         }
       </div>
