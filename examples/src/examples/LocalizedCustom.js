e1462b3818e0a56c24cbcdeb9dba52da8cd8ff72
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-10-11 20:10:41 +0200

    Update example to match fix in #220

--- examples/src/examples/LocalizedCustom.js
+++ examples/src/examples/LocalizedCustom.js
@@ -2,13 +2,12 @@ import React from 'react';
 import DayPicker from '../../../src';
 
 const weekdaysLong = {
-  // Make sure you start with the right day of the week!
-  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  ru: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
   en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
 };
 const weekdaysShort = {
   // Idem
-  ru: ['По', 'Вт', 'Ср', 'Че', 'Пя', 'Су', 'Во'],
+  ru: ['Во', 'По', 'Вт', 'Ср', 'Че', 'Пя', 'Су'],
   en: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
 };
 const months = {
@@ -25,8 +24,8 @@ const localeUtils = {
     `${weekdaysLong[locale][d.getDay()]}, ${d.getDate()} ${months[locale][d.getMonth()]} ${d.getFullYear()}`,
   formatWeekdayShort: (index, locale = 'en') => weekdaysShort[locale][index],
   formatWeekdayLong: (index, locale = 'en') => weekdaysLong[locale][index],
-  getFirstDayOfWeek: (locale) => firstDayOfWeek[locale],
-  getMonths: (locale) => months[locale],
+  getFirstDayOfWeek: locale => firstDayOfWeek[locale],
+  getMonths: locale => months[locale],
   formatMonthTitle: (d, locale) => `${months[locale][d.getMonth()]} ${d.getFullYear()}`,
 };
 
