15ac5a4407b3807a769dc19878a1be397a88beb1
Giampaolo Bellavite <gpbellavite@gmail.com>
2016-10-14 12:51:06 +0200

    Add tests for new props

--- test/DayPicker.js
+++ test/DayPicker.js
@@ -160,11 +160,6 @@ describe('<DayPicker />', () => {
       );
       expect(wrapper.find('.DayPicker-Day')).to.have.length(42);
     });
-    it('should render weekday labels accounting for locale settings', () => {
-      const localeUtils = Object.assign({}, LocaleUtils, { getFirstDayOfWeek: () => 1 });
-      const wrapper = mount(<DayPicker localeUtils={ localeUtils } />);
-      expect(wrapper.find('.DayPicker-Weekday').at(0)).to.have.text('Mo');
-    });
   });
 
   describe('day modifiers', () => {
@@ -839,6 +834,47 @@ describe('<DayPicker />', () => {
       expect(instance.state.currentMonth.getFullYear()).to.equal(2015);
       expect(instance.state.currentMonth.getMonth()).to.equal(5);
       expect(instance.state.currentMonth.getDate()).to.equal(1);
+    });
+  });
+
+  describe('when localized', () => {
+    it('should use the months prop to localize the month names', () => {
+      const wrapper = mount(
+        <DayPicker
+          initialMonth={ new Date(2015, 0) }
+          months={ ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'] }
+        />);
+      expect(wrapper.find('.DayPicker-Caption')).to.have.text('Gennaio 2015');
+    });
+    it('should use the firstDayOfWeek prop to set the first day of the week', () => {
+      const wrapper = mount(
+        <DayPicker
+          initialMonth={ new Date(2015, 0) }
+          firstDayOfWeek={ 1 }
+        />);
+      expect(wrapper.find('.DayPicker-Weekday').first()).to.have.text('Mo');
+      expect(wrapper.find('.DayPicker-Day').at(3)).to.have.text('1');
+    });
+    it('should use the weekdaysShort prop to localize the weekday names', () => {
+      const wrapper = mount(
+        <DayPicker
+          initialMonth={ new Date(2015, 0) }
+          weekdaysShort={ ['Do', 'Lu', 'Ma', 'Me', 'Gi', 'Ve', 'Sa'] }
+        />);
+      expect(wrapper.find('.DayPicker-Weekday').first()).to.have.text('Do');
+    });
+    it('should use the weekdaysLong prop to localize the weekday names', () => {
+      const wrapper = mount(
+        <DayPicker
+          initialMonth={ new Date(2015, 0) }
+          weekdaysLong={ ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato'] }
+        />);
+      expect(wrapper.find('.DayPicker-Weekday > abbr').first()).to.have.attr('title', 'Domenica');
+    });
+    it('should render weekday labels accounting for locale settings', () => {
+      const localeUtils = Object.assign({}, LocaleUtils, { getFirstDayOfWeek: () => 1 });
+      const wrapper = mount(<DayPicker localeUtils={ localeUtils } />);
+      expect(wrapper.find('.DayPicker-Weekday').first()).to.have.text('Mo');
     });
   });
 });
