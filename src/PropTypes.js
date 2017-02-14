da0f06f81435a5f62f7772d4eb077f8bc343efc0
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-02-14 17:16:51 -0300

    Add prop types to match the added modifiers

--- src/PropTypes.js
+++ src/PropTypes.js
@@ -1,10 +1,30 @@
 import { PropTypes } from 'react';
 
-export default {
+const PrimitiveTypes = {
   localeUtils: PropTypes.shape({
     formatMonthTitle: PropTypes.func,
     formatWeekdayShort: PropTypes.func,
     formatWeekdayLong: PropTypes.func,
     getFirstDayOfWeek: PropTypes.func,
   }),
+  range: PropTypes.shape({
+    from: PropTypes.instanceOf(Date),
+    to: PropTypes.instanceOf(Date),
+  }),
+  after: PropTypes.shape({
+    after: PropTypes.instanceOf(Date),
+  }),
+  before: PropTypes.shape({
+    before: PropTypes.instanceOf(Date),
+  }),
 };
+
+export const ModifierPropType = PropTypes.oneOfType([
+  PrimitiveTypes.after,
+  PrimitiveTypes.before,
+  PrimitiveTypes.range,
+  PropTypes.func,
+  PropTypes.array,
+]);
+
+export default PrimitiveTypes;
