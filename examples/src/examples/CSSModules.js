eafefb302c6b02820a6dc326a71d13035ddef449
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-04-20 08:12:44 -0500

    Add onDayClick to CSS modules example
    https://gitter.im/gpbl/react-day-picker?at=58f6670e69a692963ec1cd53

--- examples/src/examples/CSSModules.js
+++ examples/src/examples/CSSModules.js
@@ -3,6 +3,15 @@ import DayPicker from '../../../src';
 
 import styles from '../styles/cssmodules.css';
 
+function handleDayClick(day, modifiers) {
+  if (modifiers[styles.green]) {
+    console.log('This is a green day');
+  }
+  if (modifiers[styles.sunday]) {
+    console.log('This is Sunday');
+  }
+}
+
 export default function CSSModules() {
   return (
     <DayPicker
@@ -15,6 +24,7 @@ export default function CSSModules() {
         [styles.sunday]: day => day.getDay() === 0,
         [styles.green]: new Date(2017, 1, 10),
       }}
+      onDayClick={handleDayClick}
     />
   );
 }
