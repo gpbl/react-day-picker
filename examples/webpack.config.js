03c6a146ea42eba8a9bd9a108341d7cc103c212a
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-05-09 16:18:10 -0500

    Fix class name

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
           'RangeAdvanced',
           'Restricted',
           'SelectableDay',
-          'ShowToday',
+          'TodayButton',
           'SimpleCalendar',
           'SimpleInput',
           'TouchEvents',
