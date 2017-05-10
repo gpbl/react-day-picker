6fb89e75a5454d613f5d56b3d2626e8eca4e36dd
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-05-09 23:00:15 -0500

    Bump v5.5.0

--- src/DayPicker.js
+++ src/DayPicker.js
@@ -15,7 +15,7 @@ import keys from './keys';
 import PropTypes, { ModifierPropType } from './PropTypes';
 
 export default class DayPicker extends Component {
-  static VERSION = '5.4.3';
+  static VERSION = '5.5.0';
 
   static propTypes = {
     // Rendering months
