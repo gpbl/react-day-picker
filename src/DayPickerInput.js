38c260acd58205040c70dcda239daa09afa6488b
Giampaolo Bellavite <io@gpbl.org>
2017-06-09 22:36:35 -0500

    Prettify file

--- src/DayPickerInput.js
+++ src/DayPickerInput.js
@@ -75,7 +75,10 @@ export default class DayPickerInput extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.value !== this.state.value || nextProps.dayPickerProps.month !== this.state.month) {
+    if (
+      nextProps.value !== this.state.value ||
+      nextProps.dayPickerProps.month !== this.state.month
+    ) {
       this.setState(getStateFromProps(nextProps));
     }
   }
