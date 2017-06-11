6ff4779e583077ed79e747038e4ba0a8aff0f8e6
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-06-11 13:58:03 -0500

    Render day from Month component

--- src/Month.js
+++ src/Month.js
@@ -1,122 +1,205 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from './PropTypes';
 import Weekdays from './Weekdays';
-import { getWeekArray } from './Helpers';
-import { getWeekNumber } from './DateUtils';
+import Day from './Day';
 
-export default function Month({
-  classNames,
+import * as ModifiersUtils from './ModifiersUtils';
+import * as Helpers from './Helpers';
+import * as DateUtils from './DateUtils';
 
-  month,
-  months,
+export default class Month extends Component {
+  static propTypes = {
+    classNames: PropTypes.shape({
+      body: PropTypes.string.isRequired,
+      month: PropTypes.string.isRequired,
+      outside: PropTypes.string.isRequired,
+      today: PropTypes.string.isRequired,
+      week: PropTypes.string.isRequired,
+    }).isRequired,
+    tabIndex: PropTypes.number,
 
-  fixedWeeks,
-  captionElement,
-  weekdayElement,
+    month: PropTypes.instanceOf(Date).isRequired,
+    months: PropTypes.arrayOf(PropTypes.string),
 
-  locale,
-  localeUtils,
-  weekdaysLong,
-  weekdaysShort,
-  firstDayOfWeek,
+    modifiersStyles: PropTypes.object,
 
-  onCaptionClick,
-  children,
+    enableOutsideDays: PropTypes.bool,
 
-  footer,
-  showWeekNumbers,
-  onWeekClick,
-}) {
-  const captionProps = {
-    date: month,
-    classNames,
-    months,
-    localeUtils,
-    locale,
-    onClick: onCaptionClick ? e => onCaptionClick(month, e) : undefined,
+    renderDay: PropTypes.func.isRequired,
+
+    captionElement: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.func,
+      PropTypes.instanceOf(React.Component),
+    ]).isRequired,
+    weekdayElement: PropTypes.oneOfType([
+      PropTypes.element,
+      PropTypes.func,
+      PropTypes.instanceOf(React.Component),
+    ]),
+
+    footer: PropTypes.node,
+    fixedWeeks: PropTypes.bool,
+    showWeekNumbers: PropTypes.bool,
+
+    locale: PropTypes.string.isRequired,
+    localeUtils: PropTypes.localeUtils.isRequired,
+    weekdaysLong: PropTypes.arrayOf(PropTypes.string),
+    weekdaysShort: PropTypes.arrayOf(PropTypes.string),
+    firstDayOfWeek: PropTypes.number.isRequired,
+
+    onCaptionClick: PropTypes.func,
+    onDayClick: PropTypes.func,
+    onDayFocus: PropTypes.func,
+    onDayKeyDown: PropTypes.func,
+    onDayMouseEnter: PropTypes.func,
+    onDayMouseLeave: PropTypes.func,
+    onDayTouchEnd: PropTypes.func,
+    onDayTouchStart: PropTypes.func,
+    onWeekClick: PropTypes.func,
   };
-  const caption = React.isValidElement(captionElement)
-    ? React.cloneElement(captionElement, captionProps)
-    : React.createElement(captionElement, captionProps);
 
-  const weeks = getWeekArray(month, firstDayOfWeek, fixedWeeks);
+  renderDay = day => {
+    const monthNumber = this.props.month.getMonth();
+    const propModifiers = Helpers.getModifiersFromProps(this.props);
+    const dayModifiers = ModifiersUtils.getModifiersForDay(day, propModifiers);
+    if (
+      DateUtils.isSameDay(day, new Date()) &&
+      !Object.prototype.hasOwnProperty.call(
+        propModifiers,
+        this.props.classNames.today
+      )
+    ) {
+      dayModifiers.push(this.props.classNames.today);
+    }
+    if (day.getMonth() !== monthNumber) {
+      dayModifiers.push(this.props.classNames.outside);
+    }
 
-  return (
-    <div className={classNames.month} role="grid">
-      {caption}
-      <Weekdays
-        classNames={classNames}
-        weekdaysShort={weekdaysShort}
-        weekdaysLong={weekdaysLong}
-        firstDayOfWeek={firstDayOfWeek}
-        showWeekNumbers={showWeekNumbers}
-        locale={locale}
-        localeUtils={localeUtils}
-        weekdayElement={weekdayElement}
-      />
-      <div className={classNames.body} role="rowgroup">
-        {weeks.map(week => {
-          let weekNumber;
-          if (showWeekNumbers) {
-            weekNumber = getWeekNumber(week[0]);
-          }
-          return (
-            <div key={week[0].getTime()} className={classNames.week} role="row">
-              {showWeekNumbers &&
-                <div
-                  className={classNames.weekNumber}
-                  tabIndex={0}
-                  role="gridcell"
-                  onClick={e => onWeekClick(weekNumber, week, e)}
-                >
-                  {weekNumber}
-                </div>}
-              {week.map(day => children(day, month))}
-            </div>
-          );
-        })}
+    const isOutside = day.getMonth() !== monthNumber;
+    let tabIndex = null;
+    if (this.props.onDayClick && !isOutside) {
+      tabIndex = -1;
+      // Focus on the first day of the month
+      if (day.getDate() === 1) {
+        tabIndex = this.props.tabIndex;
+      }
+    }
+    const key = `${day.getFullYear()}${day.getMonth()}${day.getDate()}`;
+    const modifiers = {};
+    dayModifiers.forEach(modifier => {
+      modifiers[modifier] = true;
+    });
+
+    return (
+      <Day
+        key={`${isOutside ? 'outside-' : ''}${key}`}
+        classNames={this.props.classNames}
+        day={day}
+        modifiers={modifiers}
+        modifiersStyles={this.props.modifiersStyles}
+        empty={
+          isOutside && !this.props.enableOutsideDays && !this.props.fixedWeeks
+        }
+        tabIndex={tabIndex}
+        ariaLabel={this.props.localeUtils.formatDay(day, this.props.locale)}
+        ariaDisabled={isOutside || dayModifiers.indexOf('disabled') > -1}
+        ariaSelected={dayModifiers.indexOf('selected') > -1}
+        onClick={this.props.onDayClick}
+        onFocus={this.props.onDayFocus}
+        onKeyDown={this.props.onDayKeyDown}
+        onMouseEnter={this.props.onDayMouseEnter}
+        onMouseLeave={this.props.onDayMouseLeave}
+        onTouchEnd={this.props.onDayTouchEnd}
+        onTouchStart={this.props.onDayTouchStart}
+      >
+        {this.props.renderDay(day, modifiers)}
+      </Day>
+    );
+  };
+
+  render() {
+    const {
+      classNames,
+
+      month,
+      months,
+
+      fixedWeeks,
+      captionElement,
+      weekdayElement,
+
+      locale,
+      localeUtils,
+      weekdaysLong,
+      weekdaysShort,
+      firstDayOfWeek,
+
+      onCaptionClick,
+
+      footer,
+      showWeekNumbers,
+      onWeekClick,
+    } = this.props;
+
+    const captionProps = {
+      date: month,
+      classNames,
+      months,
+      localeUtils,
+      locale,
+      onClick: onCaptionClick ? e => onCaptionClick(month, e) : undefined,
+    };
+    const caption = React.isValidElement(captionElement)
+      ? React.cloneElement(captionElement, captionProps)
+      : React.createElement(captionElement, captionProps);
+
+    const weeks = Helpers.getWeekArray(month, firstDayOfWeek, fixedWeeks);
+
+    return (
+      <div className={classNames.month} role="grid">
+        {caption}
+        <Weekdays
+          classNames={classNames}
+          weekdaysShort={weekdaysShort}
+          weekdaysLong={weekdaysLong}
+          firstDayOfWeek={firstDayOfWeek}
+          showWeekNumbers={showWeekNumbers}
+          locale={locale}
+          localeUtils={localeUtils}
+          weekdayElement={weekdayElement}
+        />
+        <div className={classNames.body} role="rowgroup">
+          {weeks.map(week => {
+            let weekNumber;
+            if (showWeekNumbers) {
+              weekNumber = DateUtils.getWeekNumber(week[0]);
+            }
+            return (
+              <div
+                key={week[0].getTime()}
+                className={classNames.week}
+                role="row"
+              >
+                {showWeekNumbers &&
+                  <div
+                    className={classNames.weekNumber}
+                    tabIndex={0}
+                    role="gridcell"
+                    onClick={e => onWeekClick(weekNumber, week, e)}
+                  >
+                    {weekNumber}
+                  </div>}
+                {week.map(this.renderDay)}
+              </div>
+            );
+          })}
+        </div>
+        {footer &&
+          <div className={classNames.footer}>
+            {footer}
+          </div>}
       </div>
-      {footer &&
-        <div className={classNames.footer}>
-          {footer}
-        </div>}
-    </div>
-  );
+    );
+  }
 }
-
-Month.propTypes = {
-  classNames: PropTypes.shape({
-    month: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-    week: PropTypes.string.isRequired,
-  }).isRequired,
-
-  month: PropTypes.instanceOf(Date).isRequired,
-  months: PropTypes.arrayOf(PropTypes.string),
-
-  fixedWeeks: PropTypes.bool,
-  captionElement: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.func,
-    PropTypes.instanceOf(React.Component),
-  ]).isRequired,
-  weekdayElement: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.func,
-    PropTypes.instanceOf(React.Component),
-  ]),
-
-  footer: PropTypes.node,
-  showWeekNumbers: PropTypes.bool,
-  onWeekClick: PropTypes.func,
-
-  locale: PropTypes.string.isRequired,
-  localeUtils: PropTypes.localeUtils.isRequired,
-  weekdaysLong: PropTypes.arrayOf(PropTypes.string),
-  weekdaysShort: PropTypes.arrayOf(PropTypes.string),
-  firstDayOfWeek: PropTypes.number.isRequired,
-
-  onCaptionClick: PropTypes.func,
-
-  children: PropTypes.func.isRequired,
-};
