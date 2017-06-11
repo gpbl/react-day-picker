09aa14ad9223eb79988d878376cf13de0cf65ce5
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-06-11 12:13:01 -0500

    Use shouldComponentUpdate in Caption component

--- src/Caption.js
+++ src/Caption.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from './PropTypes';
 
-export default function Caption({
-  classNames,
-  date,
-  months,
-  locale,
-  localeUtils,
-  onClick,
-}) {
-  return (
-    <div className={classNames.caption} role="heading">
-      <div onClick={onClick}>
-        {months
-          ? `${months[date.getMonth()]} ${date.getFullYear()}`
-          : localeUtils.formatMonthTitle(date, locale)}
-      </div>
-    </div>
-  );
-}
+export default class Caption extends Component {
+  static propTypes = {
+    date: PropTypes.instanceOf(Date),
+    months: PropTypes.arrayOf(PropTypes.string),
+    locale: PropTypes.string,
+    localeUtils: PropTypes.localeUtils,
+    onClick: PropTypes.func,
+    classNames: PropTypes.shape({
+      caption: PropTypes.string.isRequired,
+    }).isRequired,
+  };
 
-Caption.propTypes = {
-  date: PropTypes.instanceOf(Date),
-  months: PropTypes.arrayOf(PropTypes.string),
-  locale: PropTypes.string,
-  localeUtils: PropTypes.localeUtils,
-  onClick: PropTypes.func,
-  classNames: PropTypes.shape({
-    caption: PropTypes.string.isRequired,
-  }).isRequired,
-};
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.locale !== this.props.locale ||
+      nextProps.classNames !== this.props.classNames ||
+      nextProps.date.getMonth() !== this.props.date.getMonth() ||
+      nextProps.date.getFullYear() !== this.props.date.getFullYear()
+    );
+  }
+
+  render() {
+    const {
+      classNames,
+      date,
+      months,
+      locale,
+      localeUtils,
+      onClick,
+    } = this.props;
+    return (
+      <div className={classNames.caption} role="heading">
+        <div onClick={onClick}>
+          {months
+            ? `${months[date.getMonth()]} ${date.getFullYear()}`
+            : localeUtils.formatMonthTitle(date, locale)}
+        </div>
+      </div>
+    );
+  }
+}
