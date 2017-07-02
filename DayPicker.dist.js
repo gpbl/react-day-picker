d47782657cb9f40ee2919df1b999dd9cafd0657b
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-07-02 10:18:37 -0500

    Import from src, not lib

--- DayPicker.dist.js
+++ DayPicker.dist.js
@@ -1,8 +1,8 @@
-const DayPicker = require('./lib/src/DayPicker');
+const DayPicker = require('./src/DayPicker');
 
-DayPicker.default.Input = require('./lib/src/DayPickerInput').default;
-DayPicker.default.DateUtils = require('./lib/src/DateUtils').default;
-DayPicker.default.ModifiersUtils = require('./lib/src/ModifiersUtils').default;
-DayPicker.default.LocaleUtils = require('./lib/src/LocaleUtils').default;
+DayPicker.default.Input = require('./src/DayPickerInput').default;
+DayPicker.default.DateUtils = require('./src/DateUtils').default;
+DayPicker.default.ModifiersUtils = require('./src/ModifiersUtils').default;
+DayPicker.default.LocaleUtils = require('./src/LocaleUtils').default;
 
 module.exports = DayPicker.default;
