070dbc3cd082c993da4de231ec499cfe6d6cc345
gpbl <gpbellavite@gmail.com>
2017-07-26 06:36:59 +0000

    Updates

--- node_modules/gh-pages/plugin.js
+++
@@ -1,6 +0,0 @@
-var ghPages = require('./lib/index');
-var path = require('path');
-
-module.exports = function(pluginConfig, config, callback) {
-  ghPages.publish(path.join(process.cwd(), config.basePath), config, callback);
-};
