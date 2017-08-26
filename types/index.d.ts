99507952a5939133a51a75a7f83ed12f15bbc228
Adi Dahiya <adahiya@palantir.com>
2017-08-25 20:13:50 -0400

    Add a comment

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,6 @@
 // TypeScript Version: 2.2
 
 import { DayPicker } from "./DayPicker";
+// We need to use 'export =' to be compatible with the 'module.exports = DayPicker.default || DayPicker' syntax
+// in DayPicker.js in the final NPM package.
 export = DayPicker;
