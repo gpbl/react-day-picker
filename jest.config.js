f1bbfd8f0f859d10eb1657048c97655d947d9633
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-09-30 22:53:41 +0200

    Setup enzyme 3

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  setupTestFrameworkScriptFile: './node_modules/jest-enzyme/lib/index.js',
+  setupFiles: ['raf/polyfill', './test/setup.js'],
+  testMatch: ['<rootDir>/test/**/!(setup).js'],
+};
