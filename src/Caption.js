9c8d753ede82fca5030a2a91eeb337e820a4c8ed
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-10-15 09:52:12 +0200

    Add onKeyUp events
    Fix a11y rule click-events-have-key-events

--- src/Caption.js
+++ src/Caption.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { ENTER } from './keys';
+
 export default class Caption extends Component {
   static propTypes = {
     date: PropTypes.instanceOf(Date),
@@ -13,6 +15,11 @@ export default class Caption extends Component {
     }).isRequired,
   };
 
+  constructor(props) {
+    super(props);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
   shouldComponentUpdate(nextProps) {
     return (
       nextProps.locale !== this.props.locale ||
@@ -20,6 +27,12 @@ export default class Caption extends Component {
       nextProps.date.getMonth() !== this.props.date.getMonth() ||
       nextProps.date.getFullYear() !== this.props.date.getFullYear()
     );
+  }
+
+  handleKeyUp(e) {
+    if (e.keyCode === ENTER) {
+      this.props.onClick(e);
+    }
   }
 
   render() {
@@ -33,7 +46,7 @@ export default class Caption extends Component {
     } = this.props;
     return (
       <div className={classNames.caption} role="heading">
-        <div onClick={onClick}>
+        <div onClick={onClick} onKeyUp={this.handleKeyUp}>
           {months
             ? `${months[date.getMonth()]} ${date.getFullYear()}`
             : localeUtils.formatMonthTitle(date, locale)}
