d7ead8350e456fc1746e02f9477a2fdf19dc962e
Alex Zhang <azhang@stratag.com>
2017-10-23 10:50:45 -0500

    Add modifiers back to type

--- types/DayPicker.d.ts
+++ types/DayPicker.d.ts
@@ -11,6 +11,8 @@ export class DayPicker extends React.Component<DayPickerProps, any> {
   static LocaleUtils: LocaleUtils;
   static DateUtils: DateUtils;
   static ModifiersUtils: ModifiersUtils;
+  static DayModifiers: DayModifiers;
+  static Modifiers: Modifiers;
   showMonth(month: Date): void;
   showPreviousMonth(): void;
   showNextMonth(): void;
