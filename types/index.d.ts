e3bc6dff43a2687d6fc340fc834628080b239a26
Tristan Teufel <tristanteufel@googlemail.com>
2017-11-05 13:50:35 +0100

    Update index.d.ts (#539)
    (fix typing) typescript default export

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,4 +3,4 @@
 import { DayPicker } from "./DayPicker";
 // We need to use 'export =' to be compatible with the 'module.exports = DayPicker.default || DayPicker' syntax
 // in DayPicker.js in the final NPM package.
-export = DayPicker;
+export default DayPicker;
