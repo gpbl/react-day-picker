a3f7d391f58b7c29b8670c38cec5b130228eb880
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-11-24 21:58:05 +0100

    Fix wrong imports

--- docs/src/code-samples/examples/input-custom-overlay.js
+++ docs/src/code-samples/examples/input-custom-overlay.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { formatMonthTitle } from '../../../../src/LocaleUtils';
-import DayPickerInput from '../../../../src/DayPickerInput';
-import '../../../../src/style.css';
-// import 'react-day-picker/lib/style.css';
+
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import { LocaleUtils } from 'react-day-picker';
+
+import 'react-day-picker/lib/style.css';
 
 function CustomOverlay({ classNames, selectedDay, month, children }) {
   return (
     <div className={classNames.overlayWrapper} style={{ marginLeft: -100 }}>
       <div className={classNames.overlay}>
-        <h3>{formatMonthTitle(month)}</h3>
+        <h3>{LocaleUtils.formatMonthTitle(month)}</h3>
         <p>
           {selectedDay
             ? `You picked: ${selectedDay.toLocaleDateString()}`
