f8cd51643f890872d526656a5c0521ac77e8aee9
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-11-25 12:51:42 +0100

    Remove commonjs exports

--- DayPicker.js
+++ DayPicker.js
@@ -1,18 +1,10 @@
 /* eslint-disable no-var */
 /* eslint-env node */
 
-var DayPicker = require('./lib/src/DayPicker');
-var DateUtils = require('./lib/src/DateUtils');
-var LocaleUtils = require('./lib/src/LocaleUtils');
-var ModifiersUtils = require('./lib/src/ModifiersUtils');
-var Weekday = require('./lib/src/Weekday').default;
-var Navbar = require('./lib/src/Navbar').default;
-var PropTypes = require('./lib/src/PropTypes');
-
-module.exports = DayPicker.default || DayPicker;
-module.exports.DateUtils = DateUtils.default || DateUtils;
-module.exports.LocaleUtils = LocaleUtils.default || LocaleUtils;
-module.exports.ModifiersUtils = ModifiersUtils.default || ModifiersUtils;
-module.exports.WeekdayPropTypes = Weekday.propTypes;
-module.exports.NavbarPropTypes = Navbar.propTypes;
-module.exports.PropTypes = PropTypes;
+export default from './lib/src/DayPicker';
+export DateUtils from './src/DateUtils';
+export ModifiersUtils from './src/ModifiersUtils';
+export LocaleUtils from './src/LocaleUtils';
+export { propTypes as WeekdayPropTypes } from './lib/src/Weekday';
+export { propTypes as NavbarPropTypes } from './lib/src/Navbar';
+export PropTypes from './lib/src/PropTypes';
