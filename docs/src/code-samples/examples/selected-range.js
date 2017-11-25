bee2bc478f516b4d355e6c613cea21ca0ce99d0b
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-11-25 10:26:13 +0100

    Update examples

--- docs/src/code-samples/examples/selected-range.js
+++ docs/src/code-samples/examples/selected-range.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Helmet from 'react-helmet';
 import DayPicker, { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -28,6 +28,7 @@ export default class Example extends React.Component {
   }
   render() {
     const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
     return (
       <div className="RangeExample">
         <p>
@@ -40,11 +41,31 @@ export default class Example extends React.Component {
           {from && to && <button onClick={this.handleResetClick}>Reset</button>}
         </p>
         <DayPicker
+          className="Selectable"
           numberOfMonths={this.props.numberOfMonths}
           selectedDays={[from, { from, to }]}
+          modifiers={modifiers}
           onDayClick={this.handleDayClick}
-          fixedWeeks
         />
+        <Helmet>
+          <style>{`
+  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+    background-color: #f0f8ff !important;
+    color: #4a90e2;
+  }
+  .Selectable .DayPicker-Day {
+    border-radius: 0 !important;
+  }
+  .Selectable .DayPicker-Day--start {
+    border-top-left-radius: 50% !important;
+    border-bottom-left-radius: 50% !important;
+  }
+  .Selectable .DayPicker-Day--end {
+    border-top-right-radius: 50% !important;
+    border-bottom-right-radius: 50% !important;
+  }
+`}</style>
+        </Helmet>
       </div>
     );
   }
