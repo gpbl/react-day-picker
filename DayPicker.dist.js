4e2dffdcf3e71bd6ba3268047cb6908b96a25dd8
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-11-26 19:23:34 +0100

    Fix dist exports

--- DayPicker.dist.js
+++ DayPicker.dist.js
@@ -1,8 +1,11 @@
+/*
+  Used to create /lib/daypicker.min.js (e.g. for unpkg)
+*/
+
 /* eslint-disable no-var */
 /* eslint-env node */
 
-var DayPicker = require('./src/DayPicker');
-
-DayPicker.Input = require('./src/DayPickerInput');
+var DayPicker = require('./src/DayPicker').default;
+DayPicker.Input = require('./src/DayPickerInput').default;
 
 module.exports = DayPicker;
