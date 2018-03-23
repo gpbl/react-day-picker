f54d9921f6e3077e92c9714ea5cd04f48a60c82a
Bogdan Chadkin <trysound@yandex.ru>
2018-03-23 20:41:04 +0300

    Combine UMD configs with rollup multiconfig

--- rollup.config.js
+++ rollup.config.js
@@ -2,30 +2,40 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-export default {
-  input: './src/index.js',
-  output: {
-    file: `lib/daypicker${isProduction ? '.min' : ''}.js`,
-    sourcemap: true,
-    format: 'umd',
-    name: 'DayPicker',
-    globals: {
-      react: 'React',
+const getUMDConfig = ({ env, file }) => {
+  const config = {
+    input: './src/index.js',
+    output: {
+      file,
+      sourcemap: true,
+      format: 'umd',
+      name: 'DayPicker',
+      globals: {
+        react: 'React',
+      },
     },
-  },
-  external: ['react'],
-  plugins: [
-    babel({
-      plugins: [
-        'external-helpers',
-        ['transform-react-remove-prop-types', { removeImport: true }],
-      ],
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    isProduction ? uglify() : {},
-  ],
+    external: ['react'],
+    plugins: [
+      babel({
+        plugins: [
+          'external-helpers',
+          ['transform-react-remove-prop-types', { removeImport: true }],
+        ],
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+    ],
+  };
+
+  if (env === 'production') {
+    config.plugins.push(uglify());
+  }
+
+  return config;
 };
+
+export default [
+  getUMDConfig({ env: 'development', file: 'lib/daypicker.js' }),
+  getUMDConfig({ env: 'production', file: 'lib/daypicker.min.js' }),
+];
