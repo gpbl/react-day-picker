9a24da77cf4cf3d4cf6a64d4d6701d39d2d40c5e
Giampaolo Bellavite <gpbellavite@gmail.com>
2018-05-06 17:37:21 +0200

    Improve example with date-fns
    See also #643

--- docs/src/code-samples/examples/input-date-fns.js
+++ docs/src/code-samples/examples/input-date-fns.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 import DayPickerInput from 'react-day-picker/DayPickerInput';
+import { DateUtils } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
+import dateFnsFormat from 'date-fns/format';
+import dateFnsParse from 'date-fns/parse';
+
+function parseDate(str, format, locale) {
+  const parsed = dateFnsParse(str, format, { locale });
+  if (DateUtils.isDate(parsed)) {
+    return parsed;
+  }
+  return undefined;
+}
+
+function formatDate(date, format, locale) {
+  return dateFnsFormat(date, format, { locale });
+}
 
 export default function Example() {
   const FORMAT = 'M/D/YYYY';
   return (
     <DayPickerInput
-      formatDate={format}
+      formatDate={formatDate}
       format={FORMAT}
-      parseDate={parse}
-      placeholder={`${format(new Date(), FORMAT)}`}
+      parseDate={parseDate}
+      placeholder={`${dateFnsFormat(new Date(), FORMAT)}`}
     />
   );
 }
