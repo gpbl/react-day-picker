6dd473a1945a265d36cf757492b82cdc9afd3656
Giampaolo Bellavite <gpbellavite@gmail.com>
2018-09-06 10:36:53 +0200

    Bump v7.2.3

--- src/DayPicker.js
+++ src/DayPicker.js
@@ -15,7 +15,7 @@ import classNames from './classNames';
 import { ENTER, SPACE, LEFT, UP, DOWN, RIGHT } from './keys';
 
 export class DayPicker extends Component {
-  static VERSION = '7.2.2';
+  static VERSION = '7.2.3';
 
   static propTypes = {
     // Rendering months
