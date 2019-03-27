709e80750239c7594cb6351f69c346d52981eb14
Adam Misiorny <adam.misiorny@gmail.com>
2019-03-27 18:28:50 +0100

    Add TypeScript declaration for utils.js

---
+++ utils.d.ts
@@ -0,0 +1,5 @@
+import { DateUtils, LocaleUtils, ModifiersUtils } from './types'
+
+export const localeUtils: LocaleUtils
+export const dateUtils: DateUtils
+export const modifiersUtils: ModifiersUtils
