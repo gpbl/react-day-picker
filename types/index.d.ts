c15e2bfdf25ba252a5c5b8ea2ed83c913571a284
Nivesh Ravindran <nivesh@amplified.ai>
2019-08-03 23:44:01 +1000

    Update type export to allow importing DateUtils methods

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,8 +1,12 @@
 // TypeScript Version: 2.2
 
-import DayPicker from './DayPicker';
-export default DayPicker;
+export { default } from './DayPicker';
 
+import * as UtilTypes from './utils';
 export * from './common';
 export * from './props';
 export * from './utils';
+
+export const DateUtils: UtilTypes.DateUtils;
+export const LocaleUtils: UtilTypes.LocaleUtils;
+export const ModifiersUtils: UtilTypes.ModifiersUtils;
