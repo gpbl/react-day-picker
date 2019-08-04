00b7d989d5b2df59e4305c288cc4eb8c42dff52a
Vladimir <vzh.inbox@gmail.com>
2019-08-04 12:28:19 +0600

    Fix exports and ts errors

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,11 +2,6 @@
 
 export { default } from './DayPicker';
 
-import * as UtilTypes from './utils';
 export * from './common';
 export * from './props';
 export * from './utils';
-
-export const DateUtils: UtilTypes.DateUtils;
-export const LocaleUtils: UtilTypes.LocaleUtils;
-export const ModifiersUtils: UtilTypes.ModifiersUtils;
