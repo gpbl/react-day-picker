7629558be80c37efcc0cfe9a46bef1dde30b08a8
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-10 07:44:10 -0600

    Update build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import { terser } from 'rollup-plugin-terser';
 import autoExternal from 'rollup-plugin-auto-external';
-// import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy';
 import replace from 'rollup-plugin-replace';
@@ -24,7 +23,6 @@ const sharedPlugins = [
   resolve(),
   commonjs(),
   autoExternal(),
-  stripPropTypes(),
   copy({
     targets: [{ src: 'src/style.css', dest: 'lib' }],
   }),
@@ -41,7 +39,7 @@ export default [
       sourcemap: true,
       globals: { react: 'React', 'date-fns': 'dateFns' },
     },
-    plugins: [...sharedPlugins],
+    plugins: [...sharedPlugins, terser(), sizeSnapshot(), stripPropTypes()],
   },
   {
     input,
