7bd87ed96ac2e6f52c2e0d008854e4f1fcad243c
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-12 09:31:10 -0600

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,53 +1,38 @@
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-import { terser } from 'rollup-plugin-terser';
 import autoExternal from 'rollup-plugin-auto-external';
-import commonjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy';
-import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import stripPropTypes from 'rollup-plugin-strip-prop-types';
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 
 import pkg from './package.json';
 
-const input = 'src/index.ts';
-const onwarn = warning => {
-  throw Error(warning.message);
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'auto',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'auto',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    autoExternal(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true,
+      verbosity: 2,
+    }),
+    copy({
+      targets: [{ src: 'src/style.css', dest: 'lib' }],
+    }),
+    sizeSnapshot(),
+  ],
 };
-
-const sharedPlugins = [
-  replace({
-    'process.env.NODE_ENV': JSON.stringify('development'),
-  }),
-  typescript(),
-  resolve(),
-  commonjs(),
-  autoExternal(),
-  copy({
-    targets: [{ src: 'src/style.css', dest: 'lib' }],
-  }),
-];
-
-export default [
-  // {
-  //   input,
-  //   onwarn,
-  //   output: {
-  //     name: 'DayPicker',
-  //     file: pkg.browser,
-  //     format: 'umd',
-  //     sourcemap: true,
-  //     globals: { react: 'React', 'date-fns': 'dateFns' },
-  //   },
-  //   plugins: [...sharedPlugins, terser(), sizeSnapshot(), stripPropTypes()],
-  // },
-  {
-    input,
-    onwarn,
-    output: [
-      // { file: pkg.main, format: 'cjs', sourcemap: true },
-      { file: pkg.main, format: 'es', sourcemap: true },
-    ],
-    plugins: sharedPlugins,
-  },
-];
