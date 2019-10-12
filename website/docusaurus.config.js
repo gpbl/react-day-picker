e96bd5266379c95aafa99b158f3212acb9d32fef
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-11 20:41:52 -0600

    Link

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
         src: 'images/logo.png',
       },
       links: [
-        { to: 'docs/start', label: 'Docs', position: 'left' },
+        { to: 'docs/start', label: 'Documentation', position: 'left' },
         { to: 'docs/props', label: 'Props', position: 'left' },
         { to: 'changelog', label: 'Changelog', position: 'left' },
         {
