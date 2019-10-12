edab16a6b12780fd188537dca980a04c2c67fd56
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-12 09:32:03 -0600

    Fix function

--- src/components/utils/filterUndefinedProps.ts
+++ src/components/utils/filterUndefinedProps.ts
@@ -6,6 +6,7 @@
 export function filterUndefinedProps(obj: {
   [index: string]: any;
 }): { [index: string]: any } {
+  if (!obj) return {};
   const clean: { [index: string]: any } = {};
   Object.entries(obj).forEach(([key, value]) => {
     if (typeof value === 'undefined') {
