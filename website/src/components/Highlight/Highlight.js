0a7d4d72c28bf576746c813b4a8339ddb3bc2c95
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-13 11:01:39 -0600

    Better highlight component

--- website/src/components/Highlight/Highlight.js
+++ website/src/components/Highlight/Highlight.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
@@ -7,10 +8,35 @@ import lightTheme from 'prism-react-renderer/themes/github';
 import darkTheme from 'prism-react-renderer/themes/vsDark';
 
 import useTheme from './hooks/useTheme';
+
+function renderTokens({
+  className,
+  style,
+  tokens,
+  getLineProps,
+  getTokenProps,
+}) {
+  const renderLine = (line, i) => {
+    const lineProps = getLineProps({ line, key: i });
+    return (
+      <div key={i} {...lineProps}>
+        {line.map((token, key) => (
+          <span key={key} {...getTokenProps({ token, key })} />
+        ))}
+      </div>
+    );
+  };
+  return (
+    <pre className={className} style={style}>
+      {tokens.map(renderLine)}
+    </pre>
+  );
+}
+
 /**
  * Highlight code with PrismHighlight.
  */
-function Highlight({ code, language = 'jsx', style }) {
+function Highlight({ code, language = 'jsx' }) {
   const [theme, setTheme] = useState('light');
   useTheme(newTheme => setTheme(newTheme));
   return (
@@ -19,18 +45,9 @@ function Highlight({ code, language = 'jsx', style }) {
       theme={theme === 'light' ? lightTheme : darkTheme}
       code={code.trim()}
       language={language}
+      data-line="1,4"
     >
-      {({ className, _style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, ..._style }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
+      {renderTokens}
     </PrismHighlight>
   );
 }
