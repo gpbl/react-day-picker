d7ed1f77a5981e54f7351b5739fb85d506919429
Adi Dahiya <adahiya@palantir.com>
2019-10-17 17:10:15 -0400

    Improve typings, use TypeScript 3.1 (#929)
    * Upgrade TypeScript to 3.1
    
    * Remove explicit TypeScript dependency
    
    * Remove redundant type exports
    
    * Add types/README.md

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,12 +1,6 @@
-// TypeScript Version: 2.3
+// TypeScript Version: 3.1
 
 export { default } from './DayPicker';
-
-import * as UtilTypes from './utils';
 export * from './common';
 export * from './props';
 export * from './utils';
-
-export const DateUtils: typeof UtilTypes.DateUtils;
-export const LocaleUtils: typeof UtilTypes.LocaleUtils;
-export const ModifiersUtils: typeof UtilTypes.ModifiersUtils;
