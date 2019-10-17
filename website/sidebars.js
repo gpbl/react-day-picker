5f2b80243a59a393eb3feb412e221d4d43b9fd4a
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-17 14:40:30 -0600

    Update docs

--- website/sidebars.js
+++ website/sidebars.js
@@ -2,8 +2,13 @@
 module.exports = {
   docs: {
     'Get Started': ['start', 'basics', 'styling', 'customization'],
-    Guides: ['matching-days', 'modifiers', 'localization', 'extending'],
-    Recipes: ['input'],
-    ['API Reference']: ['props', 'utils'],
+    Guides: [
+      'matching-days',
+      'modifiers',
+      'localization',
+      'input',
+      'extending',
+    ],
+    ['API Reference']: ['props', 'hooks', 'helpers'],
   },
 };
