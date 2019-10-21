87d7db52d5721b930c7bc8a5659bf72a25eebec4
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-10-21 06:55:45 -0600

    Remove unused files

--- docs/gatsby-config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass'],
-};
--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-// TypeScript Version: 3.1
-
-export { default } from './DayPicker';
-export * from './common';
-export * from './props';
-export * from './utils';
