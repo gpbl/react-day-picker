0a551b9640b5c8f3074b40a5b9f0c91ec5a8c864
Giampaolo Bellavite <gpbellavite@gmail.com>
2019-11-08 11:18:40 -0600

    Upgrade dependencies

--- package/rollup.config.js
+++ package/rollup.config.js
@@ -1,78 +1,89 @@
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-import copy from 'rollup-plugin-copy';
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import { terser } from 'rollup-plugin-terser';
-import typescript from 'rollup-plugin-typescript2';
-import path from 'path';
-import pkg from './package.json';
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+import copy from "rollup-plugin-copy";
+import resolve from "rollup-plugin-node-resolve";
+import replace from "rollup-plugin-replace";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import path from "path";
+import pkg from "./package.json";
+import alias from "@rollup/plugin-alias";
 
-const name = 'ReactDayPicker';
-const input = './src/index.ts';
-const globals = { react: 'React' };
+const name = "ReactDayPicker";
+const input = "./src/index.ts";
+const globals = { react: "React" };
 const external = Object.keys(globals);
 
 const typescriptPlugin = typescript({
   rollupCommonJSResolveHack: true,
   clean: true,
-  verbosity: 2,
+  verbosity: 2
 });
 const replaceProduction = replace({
-  'process.env.NODE_ENV': JSON.stringify('production'),
+  "process.env.NODE_ENV": JSON.stringify("production")
+});
+
+const aliasPlugin = alias({
+  entries: {
+    components: "src/components",
+    hooks: "src/hooks",
+    classes: "src/classes"
+  }
 });
 
 export default [
   {
     input,
     output: {
-      file: 'lib/index.umd.js',
-      format: 'umd',
+      file: "lib/index.umd.js",
+      format: "umd",
       name,
       globals,
-      sourcemap: true,
+      sourcemap: true
     },
     external,
-    plugins: [resolve(), typescriptPlugin],
+    plugins: [resolve(), typescriptPlugin, aliasPlugin]
   },
   {
     input,
     output: {
-      file: 'lib/index.umd.min.js',
-      format: 'umd',
+      file: "lib/index.umd.min.js",
+      format: "umd",
       name,
       globals,
-      sourcemap: true,
+      sourcemap: true
     },
     external,
     plugins: [
       resolve(),
       typescriptPlugin,
+      aliasPlugin,
       replaceProduction,
       terser(),
-      sizeSnapshot(),
-    ],
+      sizeSnapshot()
+    ]
   },
   {
     input,
     output: [
       {
         file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
+        format: "cjs",
+        sourcemap: true
       },
       {
         file: pkg.module,
-        format: 'esm',
-        sourcemap: true,
-      },
+        format: "esm",
+        sourcemap: true
+      }
     ],
-    external: id => !id.startsWith('.') && !path.isAbsolute(id),
+    external: id => !id.startsWith(".") && !path.isAbsolute(id),
     plugins: [
       resolve(),
+      aliasPlugin,
       typescriptPlugin,
       copy({
-        targets: [{ src: ['src/style.css'], dest: ['lib'] }],
-      }),
-    ],
-  },
+        targets: [{ src: ["src/style.css"], dest: ["lib"] }]
+      })
+    ]
+  }
 ];
