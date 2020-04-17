6a7710b1dd696d2909a579a075298674af5a2628
Giampaolo Bellavite <io@gpbl.dev>
2020-04-17 08:34:25 -0500

    Add webpack.config

---
+++ packages/react-day-picker/webpack.config.ts
@@ -0,0 +1,52 @@
+import * as webpack from 'webpack'
+
+const base: webpack.Configuration = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    library: 'DayPicker',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'date-fns': {
+      root: 'DateFns',
+      commonjs2: 'date-fns',
+      commonjs: 'date-fns',
+      amd: 'date-fns'
+    }
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+  }
+}
+
+const minified = {
+  ...base,
+  output: {
+    ...base.output,
+    filename: 'index.min.js'
+  }
+}
+
+const notMinified = {
+  ...base,
+  output: {
+    ...base.output,
+    filename: 'index.js'
+  },
+  optimization: {
+    minimize: false
+  }
+}
+
+export default [minified, notMinified]
