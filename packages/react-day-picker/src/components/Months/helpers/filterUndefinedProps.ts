93bb81cafd3897e373dca0349376b21e8b6aa681
gpbl <gpbellavite@icloud.com>
2020-04-20 07:22:58 -0500

    chore(v8): add eslint support (#1022)
    * Add eslint support (closes #1021)
    
    * Revert wrong changes

--- packages/react-day-picker/src/components/Months/helpers/filterUndefinedProps.ts
+++ packages/react-day-picker/src/components/Months/helpers/filterUndefinedProps.ts
@@ -3,12 +3,12 @@
  */
 export function filterUndefinedProps(obj: {
   [index: string]: any;
-}): { [index: string]: any } {
+}): { [index: string]: unknown } {
   if (!obj) return {};
-  const clean: { [index: string]: any } = {};
+  const clean: { [index: string]: unknown } = {};
 
-  Object.entries(obj).forEach(([key, value]: [string, any]) => {
-    if (typeof value === "undefined") {
+  Object.entries(obj).forEach(([key, value]: [string, unknown]) => {
+    if (typeof value === 'undefined') {
       return;
     }
     clean[key.toString()] = value;
