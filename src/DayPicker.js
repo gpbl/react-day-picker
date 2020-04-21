08989c30fbb8b2d496eddc64717153b13cf0a4c0
Giampaolo Bellavite <io@gpbl.dev>
2020-04-20 19:52:42 -0500

    Bump v7.4.7

--- src/DayPicker.js
+++ src/DayPicker.js
@@ -173,7 +173,7 @@ export class DayPicker extends Component {
     }
   }
 
-  static VERSION = '7.4.6';
+  static VERSION = '7.4.7';
 
   /**
    * Return the month to be shown in the calendar based on the component props.
