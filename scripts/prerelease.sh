8299c2f863fe6986a6a2f40de62790a633cdcecc
Giampaolo Bellavite <io@gpbl.dev>
2020-04-22 08:17:05 -0500

    Bump v8.0.0-alpha.31

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -5,19 +5,20 @@ cd packages/react-day-picker
 
 VERSION=`npm version prerelease --preid=alpha`;
 
-echo This script will pre-release react-day-picker on npm.
+echo 👋 This script will pre-release react-day-picker on npm.
 echo Version: $VERSION
 echo
-echo Committing changes...
-git commit -a -m "Bump ${VERSION}"
+echo 📡 Committing changes...
+git commit -a -m "Bump ${VERSION}" | sed 's/^/  /'
 
-echo Tagging version...
-git tag $VERSION -a -m "${VERSION}"
+echo 🏷 Tagging version...
+git tag $VERSION -a -m "${VERSION}" | sed 's/^/  /'
 
-echo Pushing changes...
-git push
+echo 📡 Pushing changes...
+git push | sed 's/^/  /'
 
-echo Pushing tags...
-git push --tags
+echo 📡 Pushing tags...
+git push --tags | sed 's/^/  /'
 
+echo 
 echo ✅ Done!
