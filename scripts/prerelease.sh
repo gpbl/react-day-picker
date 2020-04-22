c3ea5127a06953c642a7062bce1884cf91824c17
Giampaolo Bellavite <io@gpbl.dev>
2020-04-22 07:59:52 -0500

    Bump v8.0.0-alpha.29

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -7,7 +7,7 @@ VERSION=`npm version prerelease --preid=alpha`;
 
 echo Publishing $VERSION...
 
-git tag $VERSION -a -m "${VERSION}"
 git commit -a -m "Bump ${VERSION}"
+git tag $VERSION -a -m "${VERSION}"
 git push
 git push --tags
