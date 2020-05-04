df3dd37d54b986bc0002d47134680ed6ce17bc94
Giampaolo Bellavite <io@gpbl.dev>
2020-05-03 20:08:31 -0500

    Update label

--- website/config/themeConfig/navbar.js
+++ website/config/themeConfig/navbar.js
@@ -1,7 +1,7 @@
 const links = [
   { to: 'docs/start', label: 'Docs', position: 'right' },
   { to: 'docs/examples', label: 'Examples', position: 'right' },
-  { to: 'docs/reference/props', label: 'Props', position: 'right' },
+  { to: 'docs/api/daypicker', label: 'API', position: 'right' },
   { to: 'docs/reference/changelog', label: 'Changelog', position: 'right' },
   {
     href: 'https://github.com/gpbl/react-day-picker',
