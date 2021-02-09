e758017f185a3ef663a85cd2b84a44d40640f7ea
gpbl <io@gpbl.dev>
2021-02-09 18:59:00 -0500

    Fix script on Netlify

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -74,7 +74,6 @@ module.exports = {
         disableSources: true,
 
         // Markdown plugin settings
-        mdHideSources: true,
         hideProjectName: true,
         hideBreadcrumbs: true,
         hideInPageTOC: true,
