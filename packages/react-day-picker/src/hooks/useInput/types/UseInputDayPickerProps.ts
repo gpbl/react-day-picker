0b022fdb2d39adb719f38f1dffa90b8a027f530c
gpbl <io@gpbl.dev>
2021-02-11 22:42:18 -0500

    useInput: update types

--- packages/react-day-picker/src/hooks/useInput/types/UseInputDayPickerProps.ts
+++
@@ -1,8 +0,0 @@
-import { DayPickerProps } from '../../../components/DayPicker';
-
-export type UseInputDayPickerProps = {
-  onMonthChange: DayPickerProps['onMonthChange'];
-  onDayClick: DayPickerProps['onDayClick'];
-  month: DayPickerProps['month'];
-  selected: DayPickerProps['selected'];
-};
