bac27f64e1472eff96191f3f36ea11ffa4261c7a
gpbl <io@gpbl.dev>
2021-02-14 17:09:17 -0500

    Fix tabIndex not being set correctly

--- packages/react-day-picker/src/components/Day/utils/createTabIndex.ts
+++ packages/react-day-picker/src/components/Day/utils/createTabIndex.ts
@@ -6,22 +6,13 @@ export function createTabIndex(
   modifiers: ModifiersStatus,
   props: DayPickerProps
 ): number | undefined {
-  if (!modifiers.interactive) return;
-
   let tabIndex: number;
   if (props.focusedDay && isSameDay(day, props.focusedDay)) {
     tabIndex = 0;
-  } else if (isSameDay(day, new Date())) {
-    tabIndex = 0;
-  } else if (
-    !isSameDay(day, new Date()) &&
-    !isSameMonth(day, new Date()) &&
-    day.getDate() === 1
-  ) {
+  } else if (isSameMonth(day, props.month) && day.getDate() === 1) {
     tabIndex = 0;
   } else {
     tabIndex = -1;
   }
-
   return tabIndex;
 }
