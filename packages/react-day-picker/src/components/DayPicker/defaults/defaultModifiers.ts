dae405afdb1cfd6f60471a2887d33834fb9218ea
gpbl <io@gpbl.dev>
2021-02-14 17:09:26 -0500

    Cleanup code

--- packages/react-day-picker/src/components/DayPicker/defaults/defaultModifiers.ts
+++ packages/react-day-picker/src/components/DayPicker/defaults/defaultModifiers.ts
@@ -2,7 +2,7 @@ import { isSameMonth, isToday } from 'date-fns';
 import { ModifiersMatchers } from '../../../types';
 
 export const defaultModifiers: ModifiersMatchers = {
-  interactive: (day, currentMonth, props) => props.onDayClick !== undefined,
-  outside: (day, currentMonth, props) => !isSameMonth(day, currentMonth),
+  interactive: (_, __, props) => props.onDayClick !== undefined,
+  outside: (day, currentMonth) => !isSameMonth(day, currentMonth),
   today: isToday
 };
