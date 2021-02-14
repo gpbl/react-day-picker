bc694e68f86305018d445c95b7c6f388bda83eae
gpbl <io@gpbl.dev>
2021-02-13 19:52:21 -0500

    Simplify DayPickerProp interface

--- packages/react-day-picker/src/types/DayPickerProps.ts
+++ packages/react-day-picker/src/types/DayPickerProps.ts
@@ -1,48 +1,34 @@
-import { Caption, Day, Navigation, NextIcon, PrevIcon, Row } from 'components';
-
 import { DayPickerComponentProps } from './DayPickerComponentProps';
 
-/**
- * Represent the props used by DayPicker’s internal components. Extends
- * [[DayPickerComponentProps]] with the default props.
- */
-export interface DayPickerProps extends DayPickerComponentProps {
-  labelsFormatters: NonNullable<DayPickerComponentProps['labelsFormatters']>;
-  enableOutsideDaysClick: NonNullable<
-    DayPickerComponentProps['enableOutsideDaysClick']
-  >;
-  classNames: NonNullable<DayPickerComponentProps['classNames']>;
-  className: NonNullable<DayPickerComponentProps['className']>;
-  components: {
-    Caption: typeof Caption;
-    Day: typeof Day;
-    Navigation: typeof Navigation;
-    Week: typeof Row;
-    NextIcon: typeof NextIcon;
-    PrevIcon: typeof PrevIcon;
-  };
-  fixedWeeks: NonNullable<DayPickerComponentProps['fixedWeeks']>;
-  /** The day that has the focus. If set, an effect will focus the day cell. */
+type RequiredPropsNames =
+  | 'labelsFormatters'
+  | 'enableOutsideDaysClick'
+  | 'classNames'
+  | 'components'
+  | 'fixedWeeks'
+  | 'formatCaption'
+  | 'formatDay'
+  | 'formatWeekdayName'
+  | 'formatWeekNumber'
+  | 'locale'
+  | 'modifiersClassNames'
+  | 'month'
+  | 'numberOfMonths'
+  | 'pagedNavigation'
+  | 'reverseMonths'
+  | 'showCaption'
+  | 'showHead'
+  | 'showNavigation'
+  | 'showOutsideDays'
+  | 'showWeekNumber'
+  | 'today';
+
+type RequiredProps = Required<
+  Pick<DayPickerComponentProps, RequiredPropsNames>
+>;
+type OptionalProps = Omit<DayPickerComponentProps, RequiredPropsNames>;
+
+export interface DayPickerProps extends OptionalProps, RequiredProps {
+  /** The currently focused day. Used for ARIA. */
   focusedDay?: Date;
-  formatCaption: NonNullable<DayPickerComponentProps['formatCaption']>;
-  formatDay: NonNullable<DayPickerComponentProps['formatDay']>;
-  formatWeekdayName: NonNullable<DayPickerComponentProps['formatWeekdayName']>;
-  formatWeekNumber: NonNullable<DayPickerComponentProps['formatWeekNumber']>;
-  initialMonth: DayPickerComponentProps['initialMonth'];
-  locale: NonNullable<DayPickerComponentProps['locale']>;
-  modifiersClassNames: NonNullable<
-    DayPickerComponentProps['modifiersClassNames']
-  >;
-  month: NonNullable<DayPickerComponentProps['month']>;
-  numberOfMonths: NonNullable<DayPickerComponentProps['numberOfMonths']>;
-  onDayFocus?: NonNullable<DayPickerComponentProps['onDayFocus']>;
-  onDayBlur?: NonNullable<DayPickerComponentProps['onDayBlur']>;
-  pagedNavigation: NonNullable<DayPickerComponentProps['pagedNavigation']>;
-  reverseMonths: NonNullable<DayPickerComponentProps['reverseMonths']>;
-  showCaption: NonNullable<DayPickerComponentProps['showCaption']>;
-  showHead: NonNullable<DayPickerComponentProps['showHead']>;
-  showNavigation: NonNullable<DayPickerComponentProps['showNavigation']>;
-  showOutsideDays: NonNullable<DayPickerComponentProps['showOutsideDays']>;
-  showWeekNumber: NonNullable<DayPickerComponentProps['showWeekNumber']>;
-  today: NonNullable<DayPickerComponentProps['today']>;
 }
