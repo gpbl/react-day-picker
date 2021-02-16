a1f8a4355ee51774ca33f330b1fdc5a97af3057b
gpbl <io@gpbl.dev>
2021-02-16 11:27:16 -0500

    Add tsdocs

--- packages/react-day-picker/src/types/ModifiersStyles.ts
+++ packages/react-day-picker/src/types/ModifiersStyles.ts
@@ -1,6 +1,4 @@
-/**
- * Represent the inline-style assigned to each modifier.
- */
+/** The inline-style to apply to the day matching `modifier`. */
 export type ModifiersStyles = {
   [modifier: string]: React.CSSProperties;
 };
