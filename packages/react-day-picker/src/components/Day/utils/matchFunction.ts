c3e5f1103638759ba2e9eba43981a2db07ef2c1b
gpbl <io@gpbl.dev>
2021-02-20 23:42:02 -0500

    Rename function argument

--- packages/react-day-picker/src/components/Day/utils/matchFunction.ts
+++ packages/react-day-picker/src/components/Day/utils/matchFunction.ts
@@ -3,9 +3,9 @@ import { DayPickerContextValue, Matcher } from '../../../types';
 export function matchFunction(
   day: Date,
   matcher: Matcher,
-  currentMonth: Date,
+  displayMonth: Date,
   props: DayPickerContextValue
 ): boolean {
   if (!(matcher instanceof Function)) return false;
-  return matcher(day, currentMonth, props);
+  return matcher(day, displayMonth, props);
 }
