3bef8f7da3bde6f26eb16e09e7ce0fedbfbc0e4a
gpbl <io@gpbl.dev>
2021-02-26 20:34:38 -0500

    Cleanup docs, playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,16 +1,9 @@
 import './App.css';
 import 'react-day-picker/style.css';
 
-import { useState } from 'react';
-import { DayPicker, MonthChangeEventHandler } from 'react-day-picker';
-
-import { addMonths } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
 
 function Example() {
-  const [month, setMonth] = useState<Date>(addMonths(new Date(), 1));
-  const handleMonthChange: MonthChangeEventHandler = (newMonth) =>
-    setMonth(newMonth);
-  console.log(month);
   return <DayPicker captionLayout="dropdown" numberOfMonths={4} />;
 }
 
