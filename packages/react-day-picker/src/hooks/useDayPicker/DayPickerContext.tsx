d7f7eebed692aef5761a1885c4ab0feac020129b
gpbl <io@gpbl.dev>
2021-02-28 22:28:08 -0500

    Update jsdocs

--- packages/react-day-picker/src/hooks/useDayPicker/DayPickerContext.tsx
+++ packages/react-day-picker/src/hooks/useDayPicker/DayPickerContext.tsx
@@ -102,9 +102,9 @@ export interface DayPickerContextValue {
 }
 
 /**
- * The DayPickerContext is a context to share props and settings within the
- * internal components. It set the defaults values, parses some props, and
- * perform one-time calculation required to render the days.
+ * This context shares props and settings within the internal components. It set
+ * the defaults values, parses some props, and perform one-time calculation
+ * required to render the days.
  */
 export const DayPickerContext = React.createContext<
   DayPickerContextValue | undefined
