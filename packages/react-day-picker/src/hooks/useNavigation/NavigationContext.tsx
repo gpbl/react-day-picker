8fb9f55e14c62cb2d960fa32b0f2348c66c436be
gpbl <io@gpbl.dev>
2021-03-09 08:46:00 -0500

    Refactor useNavigation hook

--- packages/react-day-picker/src/hooks/useNavigation/NavigationContext.tsx
+++ packages/react-day-picker/src/hooks/useNavigation/NavigationContext.tsx
@@ -2,39 +2,22 @@ import * as React from 'react';
 
 import { isSameMonth } from 'date-fns';
 import { useDayPicker } from 'hooks';
+import { NavigationContextValue } from 'types';
 
 import { getDisplayMonths } from './utils/getDisplayMonths';
 import { getInitialMonth } from './utils/getInitialMonth';
 import { getNextMonth } from './utils/getNextMonth';
 import { getPreviousMonth } from './utils/getPreviousMonth';
 
-export type NavigationContextValue = {
-  /** The current month. Note that when `numberOfMonths > 1` represent the first month in the displayed months. */
-  month: Date;
-  /** The months to display, according to `numberOfMonths`. */
-  displayMonths: Date[];
-  /** Navigate to the specified month. */
-  setMonth: (month: Date) => void;
-  /** The next month to display. `undefined` if no months left */
-  nextMonth?: Date;
-  /** The previous month to display. `undefined` if no months left */
-  previousMonth?: Date;
-};
-
-/** The navigation context holds values and setters for navigating between the
- * calendars. */
+/** The navigation context holds values and setters for navigating between the calendars. */
 export const NavigationContext = React.createContext<
   NavigationContextValue | undefined
 >(undefined);
 
-export type NavigationProviderProps = {
-  children?: React.ReactNode;
-};
-
 /** Provides the values for the [[NavigationContext]]. */
-export const NavigationProvider = (
-  props: NavigationProviderProps
-): JSX.Element => {
+export const NavigationProvider = (props: {
+  children?: React.ReactNode;
+}): JSX.Element => {
   const context = useDayPicker();
   const initialMonth = getInitialMonth(context);
 
