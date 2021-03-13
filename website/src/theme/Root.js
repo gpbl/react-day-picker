a523847975ef22815e4db797754bd26b86d9ffa4
gpbl <io@gpbl.dev>
2021-03-13 17:46:12 -0500

    (website) Update to latest beta

--- website/src/theme/Root.js
+++ website/src/theme/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'react-day-picker/style';
+import 'react-day-picker/style.css';
 
 // Default implementation, that you can customize
 function Root({ children }) {
