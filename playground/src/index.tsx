6aed008ac521a156a1020562d98a92b46af9cb99
gpbl <io@gpbl.dev>
2021-03-18 08:28:02 -0500

    (website) Remove unused function

--- playground/src/index.tsx
+++ playground/src/index.tsx
@@ -1,10 +1,9 @@
-import './index.css';
-
 import React from 'react';
+
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,4 +15,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
