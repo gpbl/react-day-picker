7e3b95913e3a039d720cb7010a9e99259659efd7
gpbl <io@gpbl.dev>
2021-03-19 08:55:15 -0500

    (website) Add link to CodeSandbox

--- website/src/theme/CodeBlock.js
+++ website/src/theme/CodeBlock.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import 'react-day-picker/style.css';
+
+import CodeSandboxer from 'react-codesandboxer';
 import OriginalCodeBlock from '@theme-original/CodeBlock';
 
 // Default implementation, that you can customize
@@ -33,16 +35,40 @@ export default function CodeBlock(props) {
   const src = require(`!!raw-loader!../../docs/${fileName}`).default;
 
   return (
-    <>
-      <OriginalCodeBlock {...props} className="language-tsx">
+    <div className="rdp-codeblock" style={{ position: 'relative' }}>
+      <OriginalCodeBlock {...props} title="test" className="language-tsx">
         {src}
       </OriginalCodeBlock>
+      <CodeSandboxer
+        examplePath={`website/docs/${fileName}`}
+        dependencies={{
+          'react-day-picker': 'next',
+          'date-fns': 'latest',
+          '@babel/runtime': 'latest',
+          react: 'latest',
+          'react-dom': 'latest'
+        }}
+        gitInfo={{
+          account: 'gpbl',
+          branch: 'master',
+          repository: 'react-day-picker',
+          host: 'github'
+        }}
+        name={`react-day-picker: ${fileName}`}
+      >
+        {() => (
+          <button type="submit" style={{ padding: '.4em .8em' }}>
+            ↗ CodeSandbox
+          </button>
+        )}
+      </CodeSandboxer>
+
       <details open>
         <summary>Output</summary>
         <div>
           <Component />
         </div>
       </details>
-    </>
+    </div>
   );
 }
