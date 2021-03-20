fabd741651b6bafa75c356a045c0396a27f5994e
gpbl <io@gpbl.dev>
2021-03-20 18:18:06 -0500

    (chore) Add test for icon components

--- packages/react-day-picker/src/components/IconPrevious/IconPrevious.test.tsx
+++ packages/react-day-picker/src/components/IconPrevious/IconPrevious.test.tsx
@@ -1,16 +1,51 @@
 import * as React from 'react';
 
-import { RenderResult } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 
-import { customRender } from 'test';
-import { StyledComponentProps } from 'types';
+import { ContextProvider } from 'contexts';
 
 import { IconPrevious } from './IconPrevious';
 
-const setup = (props?: StyledComponentProps): RenderResult => {
-  return customRender(<IconPrevious {...props} />);
-};
-test('should render correctly', () => {
-  const { container } = setup({ className: 'foo', style: { color: 'red' } });
-  expect(container.firstChild).toMatchSnapshot();
+let icon: Element;
+let result: RenderResult;
+describe('when rendered without props', () => {
+  beforeEach(() => {
+    result = render(
+      <ContextProvider>
+        <IconPrevious />
+      </ContextProvider>
+    );
+    icon = result.container.children[0];
+  });
+  test('should render a svg element', () => {
+    expect(icon.tagName).toBe('svg');
+  });
+});
+
+describe('when using a class name from props', () => {
+  beforeEach(() => {
+    result = render(
+      <ContextProvider>
+        <IconPrevious className="foo" />
+      </ContextProvider>
+    );
+    icon = result.container.children[0];
+  });
+  test('should add the class name', () => {
+    expect(icon).toHaveClass('foo');
+  });
+});
+
+describe('when using a style from props', () => {
+  beforeEach(() => {
+    result = render(
+      <ContextProvider>
+        <IconPrevious style={{ color: 'red' }} />
+      </ContextProvider>
+    );
+    icon = result.container.children[0];
+  });
+  test('should apply the style', () => {
+    expect(icon).toHaveStyle({ color: 'red' });
+  });
 });
