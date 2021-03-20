787167874bed932f75e8c5eae5c928679fee1567
gpbl <io@gpbl.dev>
2021-03-20 16:57:16 -0500

    (website) Code sandboxer fixes

---
+++ website/src/CodeSandboxButton/files/dependencies.ts
@@ -0,0 +1,9 @@
+import { version } from 'react-day-picker/package.json';
+
+export const dependencies = {
+  '@babel/runtime': 'latest',
+  'react-day-picker': version,
+  'date-fns': 'latest',
+  'react-dom': 'latest',
+  react: 'latest'
+};
