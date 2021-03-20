07c64b0cc489aacfcfa54ee09a4c1a51e5eb2abf
gpbl <io@gpbl.dev>
2021-03-19 21:34:41 -0500

    (docs) Add style to example

--- website/docs/start.tsx
+++ website/docs/start.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/style.css';
 
 export default function App() {
   const [selected, setSelected] = React.useState<Date | undefined>();
