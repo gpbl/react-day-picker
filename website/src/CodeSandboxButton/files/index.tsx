a9776ddcecac01114ee46130cc0166138f96e946
gpbl <io@gpbl.dev>
2021-03-20 16:45:12 -0500

    (website) Remove css reset

--- website/src/CodeSandboxButton/files/index.tsx
+++ website/src/CodeSandboxButton/files/index.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import '@atlaskit/css-reset';
-
 import App from './example';
 
 ReactDOM.render(<App />, document.getElementById('root'));
