7cddf08f3e4d20c645fc4f47a386adc3fb006330
gpbl <io@gpbl.dev>
2021-03-20 18:17:59 -0500

    (chore) Add test for Footer component

--- packages/react-day-picker/src/components/Footer/Footer.test.tsx
+++ packages/react-day-picker/src/components/Footer/Footer.test.tsx
@@ -1,4 +1,26 @@
-test.todo('should render the footer content');
-describe('when no footer content', () => {
-  test.todo('should not render anything');
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import { ContextProvider } from 'contexts';
+
+import { Footer } from './Footer';
+
+test('should not render anything as default', () => {
+  const { container } = render(
+    <ContextProvider>
+      <Footer />
+    </ContextProvider>
+  );
+  expect(container).toBeEmptyDOMElement();
+});
+
+test('should render a table footer when using the `footer` props', () => {
+  render(
+    <ContextProvider footer="footer_foo">
+      <Footer />
+    </ContextProvider>
+  );
+  const label = screen.getByText(/footer_foo/i);
+  expect(label).toBeInTheDocument();
 });
