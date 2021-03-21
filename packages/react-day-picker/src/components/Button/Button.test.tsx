ee8a78188bc287edbfea2ba7c764854a6684a0a4
gpbl <io@gpbl.dev>
2021-03-20 23:36:49 -0500

    (chore) Update test for Button component

--- packages/react-day-picker/src/components/Button/Button.test.tsx
+++ packages/react-day-picker/src/components/Button/Button.test.tsx
@@ -1,63 +1,57 @@
-import { screen } from '@testing-library/react';
 import * as React from 'react';
 
-import { customRender } from 'test';
+import { customRender, PageObjects } from 'test';
 import { Button } from './Button';
 
-let button: HTMLElement;
+const po = new PageObjects(new Date());
 
 describe('when rendered without props', () => {
   beforeEach(() => {
     customRender(<Button />);
-    button = screen.getByRole('button');
   });
   test('should render a button with type "button"', () => {
-    expect(button).toHaveAttribute('type', 'button');
+    expect(po.button).toHaveAttribute('type', 'button');
   });
   test('should render a button with the button class name', () => {
-    expect(button).toHaveClass('rdp-button');
+    expect(po.button).toHaveClass('rdp-button');
   });
   test('should render a button with the reset class name', () => {
-    expect(button).toHaveClass('rdp-button_reset');
+    expect(po.button).toHaveClass('rdp-button_reset');
   });
 });
 
 describe('when using a class name from props', () => {
   beforeEach(() => {
     customRender(<Button className="foo" />);
-    button = screen.getByRole('button');
   });
   test('should add the class name', () => {
-    expect(button).toHaveClass('foo');
+    expect(po.button).toHaveClass('foo');
   });
 });
 
 describe('when using custom class names from ContextProvider props', () => {
   beforeEach(() => {
     customRender(<Button />, { classNames: { button: 'foo' } });
-    button = screen.getByRole('button');
   });
   test('should add the class name', () => {
-    expect(button).toHaveClass('foo');
+    expect(po.button).toHaveClass('foo');
   });
 });
 
 describe('when using a inline style from props', () => {
   beforeEach(() => {
     customRender(<Button style={{ color: 'blue' }} />);
-    button = screen.getByRole('button');
   });
   test('should apply the style', () => {
-    expect(button).toHaveStyle({ color: 'blue' });
+    expect(po.button).toHaveStyle({ color: 'blue' });
   });
 });
 
 describe('when using a inline style from ContextProvider props', () => {
   beforeEach(() => {
     customRender(<Button />, { styles: { button: { color: 'blue' } } });
-    button = screen.getByRole('button');
   });
   test('should apply the style', () => {
-    expect(button).toHaveStyle({ color: 'blue' });
+    expect(po.button).toHaveStyle({ color: 'blue' });
   });
 });
