310035adffa6bd22067c700dfa9f5ae78aa83d03
gpbl <io@gpbl.dev>
2021-03-21 08:02:05 -0500

    (chore) Update tests

--- packages/react-day-picker/src/components/Caption/CaptionProps.ts
+++ packages/react-day-picker/src/components/Caption/CaptionProps.ts
@@ -2,12 +2,4 @@
 export interface CaptionProps {
   /** The month where the caption is displayed. */
   displayMonth: Date;
-  /** The index of the month being displayed (when `numberOfMonths` set can be greater than `0`). */
-  displayIndex?: number;
-  /** When `numberOfMonths > 0`, whether the month is placed first. */
-  isFirst?: boolean;
-  /** When `numberOfMonths > 0`, whether the month is placed last. */
-  isLast?: boolean;
-  /**When `numberOfMonths > 0`, whether the month is placed in middle position. */
-  isMiddle?: boolean;
 }
