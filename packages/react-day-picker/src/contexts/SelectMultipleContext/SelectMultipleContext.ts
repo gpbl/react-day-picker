61bc3f2a81a3f54b40baed1425520914f36a4d02
gpbl <io@gpbl.dev>
2021-03-22 07:30:22 -0500

    (chore) Simplify SelectMultipleContext value

--- packages/react-day-picker/src/contexts/SelectMultipleContext/SelectMultipleContext.ts
+++ packages/react-day-picker/src/contexts/SelectMultipleContext/SelectMultipleContext.ts
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { SelectMultipleContextValue } from './types';
+import { SelectMultipleContextValue } from './SelectMultipleContextValue';
 
 /**
  * The SelectMultiple context shares details about the selected days when in
