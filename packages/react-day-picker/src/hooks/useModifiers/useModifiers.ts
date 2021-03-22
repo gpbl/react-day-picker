0b321aa2eb55c76e7e916e95e5752b77e89d11ca
gpbl <io@gpbl.dev>
2021-03-22 07:32:02 -0500

    (chore) Code cleanup

--- packages/react-day-picker/src/hooks/useModifiers/useModifiers.ts
+++ packages/react-day-picker/src/hooks/useModifiers/useModifiers.ts
@@ -4,55 +4,45 @@ import {
   useSelectRange,
   useSelectSingle
 } from 'contexts';
-import { ModifiersArray } from 'types';
+import {
+  isDayPickerMultiple,
+  isDayPickerRange,
+  isDayPickerSingle,
+  ModifiersArray
+} from 'types';
 import { UseModifiers } from './types';
 import { getModifierStatus } from './utils/getModifierStatus';
 
 /** Return the modifiers and its styles for the specified date. */
 export function useModifiers(date: Date): UseModifiers {
   const context = useDayPicker();
-  const { isSingleMode, ...single } = useSelectSingle();
-  const { isMultipleMode, ...multiple } = useSelectMultiple();
-  const { isRangeMode, ...range } = useSelectRange();
 
-  const today = !context.hideToday ? context.today : undefined;
+  const singleSelect = useSelectSingle();
+  const multipleSelect = useSelectMultiple();
+  const rangeSelect = useSelectRange();
 
-  // Add to the modifiers in context those coming from selection
+  // TODO: modifiers from context should not be overridden
   const modifiers: ModifiersArray = {
     ...context.modifiers,
-    today: context.modifiers.today ?? [today],
+    today: context.modifiers.today ?? [context.today],
     disabled: context.modifiers.disabled
   };
 
-  if (isSingleMode) {
-    if (single.modifiers.selected) {
-      modifiers.selected = single.modifiers.selected;
-    }
-  } else if (isMultipleMode) {
-    if (multiple.modifiers.selected) {
-      modifiers.selected = multiple.modifiers.selected;
-    }
-    if (multiple.modifiers.disabled) {
-      modifiers.disabled = modifiers.disabled.concat(
-        multiple.modifiers.disabled
-      );
-    }
-  } else if (isRangeMode) {
-    if (range.modifiers.selected) {
-      modifiers.selected = range.modifiers.selected;
-    }
-    if (range.modifiers.range_start) {
-      modifiers.range_start = range.modifiers.range_start;
-    }
-    if (range.modifiers.range_middle) {
-      modifiers.range_middle = range.modifiers.range_middle;
-    }
-    if (range.modifiers.range_end) {
-      modifiers.range_end = range.modifiers.range_end;
-    }
-    if (range.modifiers.disabled) {
-      modifiers.disabled = modifiers.disabled.concat(range.modifiers.disabled);
-    }
+  if (isDayPickerSingle(context)) {
+    modifiers.selected = singleSelect.modifiers.selected ?? [];
+  } else if (isDayPickerMultiple(context)) {
+    modifiers.selected = multipleSelect.modifiers.selected ?? [];
+    modifiers.disabled = modifiers.disabled.concat(
+      multipleSelect.modifiers.disabled ?? []
+    );
+  } else if (isDayPickerRange(context)) {
+    modifiers.selected = rangeSelect.modifiers.selected ?? [];
+    modifiers.range_start = rangeSelect.modifiers.range_start ?? [];
+    modifiers.range_middle = rangeSelect.modifiers.range_middle ?? [];
+    modifiers.range_end = rangeSelect.modifiers.range_end ?? [];
+    modifiers.disabled = modifiers.disabled.concat(
+      rangeSelect.modifiers.disabled ?? []
+    );
   }
 
   const status = getModifierStatus(date, modifiers);
