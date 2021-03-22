452408a1a13836c88630deb0e156f2c1c1762715
gpbl <io@gpbl.dev>
2021-03-21 19:40:19 -0500

    (chore) Cleanup root component

--- packages/react-day-picker/src/components/Root/Root.tsx
+++ packages/react-day-picker/src/components/Root/Root.tsx
@@ -33,30 +33,21 @@ export function Root(): JSX.Element {
   const renderMonth = (displayMonth: Date, displayIndex: number) => {
     const className = [classNames.month];
     const style = { ...styles.month };
-    let isFirst = numberOfMonths > 1 && displayIndex === 0;
-    let isLast =
-      numberOfMonths > 1 && displayIndex === displayMonths.length - 1;
+    let isFirst = displayIndex === 0;
+    let isLast = displayIndex === displayMonths.length - 1;
 
     if (dir === 'rtl') [isLast, isFirst] = [isFirst, isLast];
-
-    const isMiddle = numberOfMonths > 1 && !isFirst && !isLast;
 
     if (isFirst) {
       className.push(classNames.caption_first);
       Object.assign(style, styles.caption_first);
     }
     if (isLast) className.push(classNames.caption_last);
-    if (isMiddle) className.push(classNames.caption_middle);
+    if (!isFirst && !isLast) className.push(classNames.caption_middle);
 
     return (
       <div key={displayIndex} className={className.join(' ')} style={style}>
-        <Caption
-          displayMonth={displayMonth}
-          displayIndex={displayIndex}
-          isFirst={isFirst}
-          isLast={isLast}
-          isMiddle={isMiddle}
-        />
+        <Caption displayMonth={displayMonth} />
         <Table displayMonth={displayMonth} />
       </div>
     );
