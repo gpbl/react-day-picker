6935b322274cb2012806941ff5165ed0279ea58f
nate pappenhagen <natepappenhagen@gmail.com>
2021-05-17 09:38:54 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default } from './DayPicker';
 export { default as DateUtils } from './DateUtils';
 export { default as LocaleUtils } from './LocaleUtils';
 export { default as ModifiersUtils } from './ModifiersUtils';
+export { default as DayPickerInput } from './DayPickerInput';
