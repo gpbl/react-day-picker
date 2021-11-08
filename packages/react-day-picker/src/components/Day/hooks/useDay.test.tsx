3a949bea227324ad9622667c6fc81795b30a510d
tjfred35 <tim@toasttab.com>
2021-11-08 14:21:51 -0500

    Update the test to use customRenderHook

--- packages/react-day-picker/src/components/Day/hooks/useDay.test.tsx
+++ packages/react-day-picker/src/components/Day/hooks/useDay.test.tsx
@@ -2,52 +2,48 @@ import * as React from 'react';
 
 import tk from 'timekeeper';
 
-import { customRender } from 'test';
-
-import { useDay } from './useDay';
+import { useDay, UseDay } from './useDay';
 import { enUS } from 'date-fns/locale';
+import { customRenderHook } from 'test';
+import { DayPickerProps } from 'types';
 
-const today = new Date(2021, 8, 1);
+const insideDay = new Date(2021, 8, 1);
+const displayMonth = insideDay;
+// A date that is not in the current month:
 const outsideDay = new Date(2021, 7, 31);
 
 beforeEach(() => {
-  tk.freeze(today);
-  tk.freeze(outsideDay);
+  tk.freeze(insideDay);
 });
 afterEach(() => tk.reset());
 
-const IsOutsideTestComponent = ({
-  date,
-  displayMonth
-}: {
-  date: Date;
-  displayMonth: Date;
-}) => {
-  const buttonRef = React.useRef<HTMLButtonElement>(null);
-  const day = useDay(date, displayMonth, buttonRef);
-  const { isOutside } = day;
-  return <button ref={buttonRef}>{isOutside.toString()}</button>;
+const runUseDayTest = (
+  date: Date,
+  month: Date,
+  contextValue: DayPickerProps = {}
+): UseDay => {
+  const { result } = customRenderHook(() => {
+    const buttonRef = React.useRef<HTMLButtonElement>(null);
+    return useDay(date, month, buttonRef);
+  }, contextValue);
+  return result.current;
 };
 
-describe('test props', () => {
-  test('isOutside should be set to false for today (the first day of the month)', () => {
-    const { getByRole } = customRender(
-      <IsOutsideTestComponent date={today} displayMonth={today} />,
-      {
-        showOutsideDays: true,
-        locale: enUS
-      }
-    );
-    expect(getByRole('button')).toHaveTextContent('false');
+describe('when we use a day in the current month', () => {
+  const day = runUseDayTest(insideDay, displayMonth, {
+    showOutsideDays: true,
+    locale: enUS
   });
-  test('isOutside should be set to true for a day outside the current month', () => {
-    const { getByRole } = customRender(
-      <IsOutsideTestComponent date={outsideDay} displayMonth={today} />,
-      {
-        showOutsideDays: true,
-        locale: enUS
-      }
-    );
-    expect(getByRole('button')).toHaveTextContent('true');
+  test('isOutside should be set to false', () => {
+    expect(day.isOutside).toBeFalsy();
+  });
+});
+describe('when we use a day outside the current month', () => {
+  test('isOutside should be set to true', () => {
+    const day = runUseDayTest(outsideDay, displayMonth, {
+      showOutsideDays: true,
+      locale: enUS
+    });
+    expect(day.isOutside).toBeTruthy();
   });
 });
