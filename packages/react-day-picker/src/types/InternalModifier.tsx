75f7e527beddb036bd64af0643c60c4fef66ae25
gpbl <io@gpbl.dev>
2021-11-13 06:44:05 -0500

    Fix type

--- packages/react-day-picker/src/types/InternalModifier.tsx
+++ packages/react-day-picker/src/types/InternalModifier.tsx
@@ -1,9 +1,6 @@
 // TODO: move to enum
 /** Represent the modifiers that are used internally in DayPicker. */
-export type InternalModifier =
-  | 'selected'
-  | 'disabled'
-  | 'hidden'
-  | 'range_start'
-  | 'range_end'
-  | 'range_middle';
+export type InternalModifier = 'selected' | 'disabled' | 'hidden';
+// | 'range_start'
+// | 'range_end'
+// | 'range_middle';
