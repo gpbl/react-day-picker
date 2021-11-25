9c4f72e6d1546d045c888721e34a4f55f0773499
gpbl <io@gpbl.dev>
2021-11-25 05:26:26 -0500

    Update docs

--- website/examples/modifiers-classnames.tsx
+++ website/examples/modifiers-classnames.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { DayPicker } from "react-day-picker";
+import React from 'react';
+import { DayPicker } from 'react-day-picker';
 
 export function Example() {
   const bookedDays = [new Date(2021, 5, 8), new Date(2021, 5, 9)];
@@ -8,9 +8,7 @@ export function Example() {
 
   return (
     <>
-      <head>
-        <style>{style}</style>
-      </head>
+      <style>{style}</style>
       <DayPicker
         defaultMonth={bookedDays[0]}
         modifiers={{
