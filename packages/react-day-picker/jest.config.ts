497def585fa398f056b2eaefd9e905fbd93c1e21
gpbl <io@gpbl.dev>
2021-11-25 06:31:39 -0500

    Cleanup actions

--- packages/react-day-picker/jest.config.ts
+++ packages/react-day-picker/jest.config.ts
@@ -4,13 +4,6 @@ export default {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^test$': '<rootDir>/src/test',
-    '^types$': '<rootDir>/src/types',
-    '^components/(.*)$': '<rootDir>/src/components/$1',
-    '^hooks(.*)': '<rootDir>/src/hooks$1',
-    '^contexts(.*)': '<rootDir>/src/contexts$1'
-  },
   coverageReporters: ['lcov', 'text', 'clover'],
   setupFilesAfterEnv: ['./src/test/setup.ts']
 };
