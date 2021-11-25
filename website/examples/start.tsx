dd748c5851f53e52d20326c7cb4d9c874c934cf1
gpbl <io@gpbl.dev>
2021-11-24 20:46:04 -0500

    Restore original start.tsx example

---
+++ website/examples/start.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { DayPicker } from "react-day-picker";
+
+export function Example() {
+  const [selected, setSelected] = React.useState<Date>();
+
+  let footer = "Please pick a day.";
+  if (selected) {
+    footer = `You picked ${selected.toLocaleDateString()}.`;
+  }
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={footer}
+    />
+  );
+}
