787a637d4db8b62810cfaff79b85a975d10109a6
gpbl <io@gpbl.dev>
2021-11-24 21:16:15 -0500

    Cleanup navigation disabled example

--- website/examples/navigation-disabled.tsx
+++ website/examples/navigation-disabled.tsx
@@ -2,12 +2,5 @@ import React from "react";
 import { DayPicker } from "react-day-picker";
 
 export function Example() {
-  return (
-    <DayPicker
-      fromYear={2015}
-      toYear={2025}
-      disableNavigation
-      mode="single"
-    />
-  );
+  return <DayPicker disableNavigation />;
 }
