b5d6a629f5d73b9414739498c62401c628ce215e
gpbl <io@gpbl.dev>
2021-11-26 12:21:22 -0500

    Update jest config

--- website/jest.config.ts
+++ website/jest.config.ts
@@ -1,9 +1,16 @@
-export default {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   roots: ['./src', './examples'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@site/(.*)': '<rootDir>/$1'
   },
   testEnvironment: 'jsdom',
   coverageReporters: ['lcov', 'text', 'clover'],
   setupFilesAfterEnv: ['./src/test/setup.ts']
 };
+
+export default config;
