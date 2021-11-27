40f37aa08c2a08cb037f0364e54d67721479c826
gpbl <io@gpbl.dev>
2021-11-26 19:15:45 -0500

    Add test

---
+++ website/examples/localization-spanish.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import tk from "timekeeper";
+
+import Example from "./localization-spanish";
+import { getMonthCaption } from "@site/src/test";
+
+let container: HTMLElement;
+
+beforeEach(() => {
+  const renderResult = render(<Example />);
+  container = renderResult.container;
+});
+
+describe("when displaying November 2021", () => {
+  const today = new Date(2021, 10, 25);
+  beforeAll(() => tk.freeze(today));
+  afterAll(() => tk.reset());
+  test("should localize the caption in Spanish", () => {
+    expect(getMonthCaption(container)).toHaveTextContent(
+      "noviembre 2021"
+    );
+  });
+});
