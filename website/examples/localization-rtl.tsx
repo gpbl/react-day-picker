ae51c145fdeca95bfd44ab9f8983c6cc54e28a57
gpbl <io@gpbl.dev>
2021-11-26 19:05:27 -0500

    Add test for rtl
    Here we can try the issue described in #1289.

--- website/examples/localization-rtl.tsx
+++ website/examples/localization-rtl.tsx
@@ -4,5 +4,5 @@ import { DayPicker } from "react-day-picker";
 import arabic from "date-fns/locale/ar-SA";
 
 export default function App() {
-  return <DayPicker dir="rtl" locale={arabic} />;
+  return <DayPicker dir="rtl" locale={arabic} mode="single" />;
 }
