b8025ce0c89fb72cb10d76bac9d35b3f1893d757
gpbl <io@gpbl.dev>
2021-11-27 09:16:30 -0500

    Update test

--- website/examples/navigation-keyboard.tsx
+++ website/examples/navigation-keyboard.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { DayPicker } from 'react-day-picker';
 
-export default function Example(props: { dir?: string }) {
+export default function Example({ dir = 'rtl' }) {
   const [selected, setSelected] = React.useState<Date>();
   return (
     <DayPicker
-      dir={props.dir}
+      dir={dir}
       mode="single"
       selected={selected}
       onSelect={setSelected}
