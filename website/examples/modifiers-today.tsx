d7c27a8615b935fc7d78154f4a72ec4c2b53a28b
gpbl <io@gpbl.dev>
2021-11-26 20:10:18 -0500

    Improve tests

--- website/examples/modifiers-today.tsx
+++ website/examples/modifiers-today.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DayClickEventHandler, DayPicker } from 'react-day-picker';
 
 export default function App() {
+  const initialFooter = 'Try clicking the today’s date.';
+  const [footer, setFooter] = useState(initialFooter);
+
   const handleDayClick: DayClickEventHandler = (day, modifiers) => {
     if (modifiers.today) {
-      alert('You clicked the today’s date!');
+      setFooter('You clicked the today’s date.');
     } else {
-      alert(`You clicked ${day.toLocaleDateString()}.`);
+      setFooter(initialFooter);
     }
   };
+
   return (
-    <DayPicker
-      onDayClick={handleDayClick}
-      footer="Try clicking the today’s date."
-    />
+    <DayPicker mode="custom" onDayClick={handleDayClick} footer={footer} />
   );
 }
