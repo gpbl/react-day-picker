227771b4c379bf75da9c30a9ff77f1f9c0daba7f
gpbl <io@gpbl.dev>
2021-12-03 19:12:06 -0500

    chore: export Button as function, not as const

--- packages/react-day-picker/src/components/Button/Button.tsx
+++ packages/react-day-picker/src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { LegacyRef } from 'react';
 
 import { useDayPicker } from '../../contexts/DayPicker';
 
@@ -8,29 +8,29 @@ export type ButtonProps = React.HTMLProps<HTMLButtonElement>;
 /**
  * Render a button HTML element applying the reset class name.
  */
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => {
-    const { classNames, styles } = useDayPicker();
+export function Button(
+  props: ButtonProps & { ref?: LegacyRef<HTMLButtonElement> }
+): JSX.Element {
+  const { classNames, styles } = useDayPicker();
 
-    const classNamesArr = [classNames.button_reset, classNames.button];
-    if (props.className) {
-      classNamesArr.push(props.className);
-    }
-    const className = classNamesArr.join(' ');
-
-    const style = { ...styles.button_reset, ...styles.button };
-    if (props.style) {
-      Object.assign(style, props.style);
-    }
-
-    return (
-      <button
-        {...props}
-        ref={ref}
-        type="button"
-        className={className}
-        style={style}
-      />
-    );
+  const classNamesArr = [classNames.button_reset, classNames.button];
+  if (props.className) {
+    classNamesArr.push(props.className);
   }
-);
+  const className = classNamesArr.join(' ');
+
+  const style = { ...styles.button_reset, ...styles.button };
+  if (props.style) {
+    Object.assign(style, props.style);
+  }
+
+  return (
+    <button
+      {...props}
+      ref={props.ref}
+      type="button"
+      className={className}
+      style={style}
+    />
+  );
+}
