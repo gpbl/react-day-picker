ae2523e0ea9464676a273d349a496247ed341642
gpbl <io@gpbl.dev>
2021-12-20 07:47:38 -0500

    Add log

--- website/examples/numbering-system.tsx
+++ website/examples/numbering-system.tsx
@@ -13,8 +13,10 @@ const NU_LOCALE = 'ar-u-nu-arab';
 const formatDay: DateFormatter = (day) =>
   day.getDate().toLocaleString(NU_LOCALE);
 
-const formatWeekNumber: WeekNumberFormatter = (weekNumber) =>
-  weekNumber.toLocaleString(NU_LOCALE);
+const formatWeekNumber: WeekNumberFormatter = (weekNumber) => {
+  console.log(weekNumber, weekNumber.toLocaleString(NU_LOCALE));
+  return weekNumber.toLocaleString(NU_LOCALE);
+};
 
 const formatCaption: DateFormatter = (date, options) => {
   const y = date.getFullYear().toLocaleString(NU_LOCALE);
