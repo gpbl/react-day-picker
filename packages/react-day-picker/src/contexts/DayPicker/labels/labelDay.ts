0742472075e7bd609bbce319c1cc58e41df957da
gpbl <io@gpbl.dev>
2022-01-02 07:42:29 -0500

    chore: add labels tests
    Signed-off-by: gpbl <io@gpbl.dev>

--- packages/react-day-picker/src/contexts/DayPicker/labels/labelDay.ts
+++ packages/react-day-picker/src/contexts/DayPicker/labels/labelDay.ts
@@ -1,10 +1,9 @@
 import { format } from 'date-fns';
-
 import { DayLabel } from 'types/Labels';
 
 /**
  * The default ARIA label for the day button.
  */
-export const labelDay: DayLabel = (day, modifers, options): string => {
+export const labelDay: DayLabel = (day, modifiersStatus, options): string => {
   return format(day, 'do MMMM (EEEE)', options);
 };
--- packages/react-day-picker/src/contexts/DayPicker/labels/labelWeekday.test.ts
+++ packages/react-day-picker/src/contexts/DayPicker/labels/labelWeekday.test.ts
@@ -1,1 +1,15 @@
-test.todo('should return the formatted weekday label');
+import es from 'date-fns/locale/es';
+
+import { labelWeekday } from './labelWeekday';
+
+const weekDay = new Date(2022, 10, 21);
+
+test('should return the formatted weekday name', () => {
+  expect(labelWeekday(weekDay)).toEqual('Monday');
+});
+
+describe('when a locale is passed in', () => {
+  test('should format using the locale', () => {
+    expect(labelWeekday(weekDay, { locale: es })).toEqual('lunes');
+  });
+});
