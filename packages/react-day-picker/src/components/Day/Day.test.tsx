5f234dfb2457b6ef3c2518400a731e5c5111ca48
gpbl <io@gpbl.dev>
2022-01-08 06:27:06 -0500

    chore(test): add test for Day component
    Signed-off-by: gpbl <io@gpbl.dev>

--- packages/react-day-picker/src/components/Day/Day.test.tsx
+++ packages/react-day-picker/src/components/Day/Day.test.tsx
@@ -1,9 +1,69 @@
-describe('when the day to render is hidden', () => {
-  test.todo('should render an empty fragment');
+import React from 'react';
+
+import { customRender } from 'test/render';
+import { freezeBeforeAll } from 'test/utils';
+
+import { DayPickerProps } from 'types/DayPicker';
+
+import { Day, DayProps } from './Day';
+
+const today = new Date(2021, 8);
+
+freezeBeforeAll(today);
+let container: HTMLElement;
+function setup(props: DayProps, dayPickerProps?: DayPickerProps) {
+  const result = customRender(<Day {...props} />, dayPickerProps);
+  container = result.container;
+}
+
+const date = today;
+const displayMonth = today;
+const props: DayProps = {
+  date: date,
+  displayMonth
+};
+
+describe('when the day to render has an hidden modifier', () => {
+  const dayPickerProps: DayPickerProps = {
+    modifiers: { hidden: date }
+  };
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('should render an empty element', () => {
+    expect(container).toBeEmptyDOMElement();
+  });
 });
-describe('when the day to render is not a `button`', () => {
-  test.todo('should render a `div`');
+describe('when a no selection mode and no "onDayClick"', () => {
+  const dayPickerProps: DayPickerProps = {};
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('should render a div', () => {
+    expect(container.firstChild?.nodeName).toBe('DIV');
+  });
 });
-describe('when the day to render is a `button`', () => {
-  test.todo('should render a `button`');
+
+describe('when a selection mode is set', () => {
+  const dayPickerProps: DayPickerProps = {
+    mode: 'single'
+  };
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('should render a button', () => {
+    expect(container.firstChild?.nodeName).toBe('BUTTON');
+  });
+});
+
+describe('when "onDayClick" is present', () => {
+  const dayPickerProps: DayPickerProps = {
+    onDayClick: jest.fn()
+  };
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('should render a button', () => {
+    expect(container.firstChild?.nodeName).toBe('BUTTON');
+  });
 });
