08d44f7ff86e79451805cd2698040cd9e2889671
gpbl <io@gpbl.dev>
2022-01-08 06:57:47 -0500

    chore(test): add test for day content
    Signed-off-by: gpbl <io@gpbl.dev>

--- packages/react-day-picker/src/components/DayContent/DayContent.test.tsx
+++ packages/react-day-picker/src/components/DayContent/DayContent.test.tsx
@@ -1,10 +1,51 @@
-test.todo('should render the hidden aria-label');
-test.todo('should render the aria-label');
-test.todo('should format the content');
+import React from 'react';
 
-describe('when is outside and cannot show outside days', () => {
-  test.todo('should not render anything');
-});
-describe('if a modifier is hidden', () => {
-  test.todo('should not render anything');
+import es from 'date-fns/locale/es';
+
+import { customRender } from 'test/render';
+import { freezeBeforeAll } from 'test/utils';
+
+import { DayContent, DayContentProps } from 'components/DayContent';
+import { defaultClassNames } from 'contexts/DayPicker/defaultClassNames';
+import { DayPickerProps } from 'types/DayPicker';
+
+const today = new Date(2021, 8);
+
+freezeBeforeAll(today);
+let container: HTMLElement;
+function setup(props: DayContentProps, dayPickerProps?: DayPickerProps) {
+  const result = customRender(<DayContent {...props} />, dayPickerProps);
+  container = result.container;
+}
+
+const date = today;
+const displayMonth = today;
+const props: DayContentProps = {
+  date: date,
+  displayMonth,
+  activeModifiers: {}
+};
+
+const dayPickerProps: DayPickerProps = {
+  locale: es
+};
+
+describe('when rendered', () => {
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('the first element is an aria-hidden element with the formatted day', () => {
+    expect(container.firstChild).toHaveAttribute('aria-hidden', 'true');
+  });
+  test('the first element contains the formatted day', () => {
+    expect(container.firstChild).toHaveTextContent('1');
+  });
+  test('the second element is visually hidden', () => {
+    expect(container.childNodes[1]).toHaveClass(defaultClassNames.vhidden);
+  });
+  test('the second element contains the formatted day', () => {
+    expect(container.childNodes[1]).toHaveTextContent(
+      '1º septiembre (miércoles)'
+    );
+  });
 });
