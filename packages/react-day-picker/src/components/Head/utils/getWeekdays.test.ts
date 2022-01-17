5740b1c2492588ddd26c8e1a385fc038b29b62d2
gpbl <io@gpbl.dev>
2022-01-17 15:44:21 -0500

    chore(test): add getWeekdays unit test
    Signed-off-by: gpbl <io@gpbl.dev>

--- packages/react-day-picker/src/components/Head/utils/getWeekdays.test.ts
+++ packages/react-day-picker/src/components/Head/utils/getWeekdays.test.ts
@@ -1,1 +1,22 @@
-test.todo('should return a week');
+import { freezeBeforeAll } from 'test/utils';
+
+import { getWeekdays } from './getWeekdays';
+
+const today = new Date(2022, 1, 12);
+const prevSunday = new Date(2022, 1, 6);
+
+freezeBeforeAll(today);
+
+let result: Date[];
+
+describe('when rendered without a locale', () => {
+  beforeEach(() => {
+    result = getWeekdays();
+  });
+  test('should return 7 days', () => {
+    expect(result).toHaveLength(7);
+  });
+  test('should return Sunday as first day', () => {
+    expect(result[0]).toEqual(prevSunday);
+  });
+});
