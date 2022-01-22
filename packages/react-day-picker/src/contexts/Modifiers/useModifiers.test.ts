e6f0f07e26ee9354445a4877e08fcf744ef6c78b
gpbl <io@gpbl.dev>
2022-01-22 13:32:07 -0500

    chore(test): complete test for useModifiers
    Signed-off-by: gpbl <io@gpbl.dev>

---
+++ packages/react-day-picker/src/contexts/Modifiers/useModifiers.test.ts
@@ -0,0 +1,46 @@
+import { RenderResult } from '@testing-library/react-hooks';
+
+import { customRenderHook } from 'test/render';
+
+import { useModifiers } from 'contexts/Modifiers';
+import { DayPickerProps } from 'types/DayPicker';
+import { DayModifiers, InternalModifier, Modifiers } from 'types/Modifiers';
+
+let renderResult: RenderResult<Modifiers>;
+
+function setup(dayPickerProps: DayPickerProps) {
+  const { result } = customRenderHook(() => useModifiers(), dayPickerProps);
+  renderResult = result;
+}
+
+const internalModifiers = Object.values(InternalModifier);
+
+describe('when rendered with custom modifiers', () => {
+  const modifier = new Date(2018, 11, 12);
+  const dayModifiers: DayModifiers = {
+    foo: modifier,
+    today: modifier,
+    outside: modifier,
+    disabled: modifier,
+    selected: modifier,
+    hidden: modifier,
+    range_start: modifier,
+    range_end: modifier,
+    range_middle: modifier
+  };
+  beforeEach(() => {
+    setup({ modifiers: dayModifiers });
+  });
+
+  test('should return the custom modifiers', () => {
+    expect(renderResult.current.foo).toEqual([dayModifiers.foo]);
+  });
+  test.each(internalModifiers)(
+    'should override the %s internal modifier',
+    (internalModifier) => {
+      expect(renderResult.current[internalModifier]).toEqual([
+        dayModifiers[internalModifier]
+      ]);
+    }
+  );
+});
