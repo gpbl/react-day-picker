2ae0519b6957b8f542f35f36a9f65d54f219bf03
gpbl <io@gpbl.dev>
2022-02-12 07:58:55 -0500

    chore(test): complete navigation test
    commit f72746d4df7fdb6402768a0371378e617c46e141
    Author: gpbl <io@gpbl.dev>
    Date:   Sat Feb 12 07:57:03 2022 -0500
    
        Add test
    
        Signed-off-by: gpbl <io@gpbl.dev>
    
    commit eeb1afdc3cb8d0b32c052494e6434ba4367497b9
    Author: gpbl <io@gpbl.dev>
    Date:   Tue Feb 8 10:12:16 2022 -0500
    
        last
    
        Signed-off-by: gpbl <io@gpbl.dev>
    
    Signed-off-by: gpbl <io@gpbl.dev>

---
+++ packages/react-day-picker/src/components/Navigation/Navigation.test.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+
+import userEvent from '@testing-library/user-event';
+import { DayPickerProps } from 'index';
+
+import { getNextButton, getPrevButton } from 'test/po';
+import { customRender } from 'test/render';
+
+import { NavigationProps } from './';
+import { Navigation } from './Navigation';
+
+let root: HTMLElement;
+
+function setup(props: NavigationProps, dayPickerProps?: DayPickerProps) {
+  const renderResult = customRender(<Navigation {...props} />, dayPickerProps);
+  root = renderResult.container.firstChild as HTMLElement;
+}
+
+const props: NavigationProps = {
+  previousMonth: new Date(2021, 3),
+  nextMonth: new Date(2021, 5),
+  displayMonth: new Date(2021, 4),
+  hidePrevious: false,
+  hideNext: false,
+  onNextClick: jest.fn(),
+  onPreviousClick: jest.fn()
+};
+
+const dayPickerProps = {
+  classNames: {
+    nav: 'foo'
+  },
+  styles: {
+    nav: { color: 'red' }
+  },
+  components: {
+    IconRight: () => <svg>IconRight</svg>,
+    IconLeft: () => <svg>IconLeft</svg>
+  }
+};
+
+describe('when rendered', () => {
+  beforeEach(() => {
+    setup(props, dayPickerProps);
+  });
+  test('should add the class name', () => {
+    expect(root).toHaveClass(dayPickerProps.classNames.nav);
+  });
+  test('should apply the style', () => {
+    expect(root).toHaveStyle(dayPickerProps.styles.nav);
+  });
+  test('the previous button should display the left icon', () => {
+    const icons = root.getElementsByTagName('svg');
+    expect(icons[0]).toHaveTextContent('IconLeft');
+  });
+  test('the next button should display the right icon', () => {
+    const icons = root.getElementsByTagName('svg');
+    expect(icons[1]).toHaveTextContent('IconRight');
+  });
+  beforeEach(() => {
+    userEvent.click(getPrevButton());
+  });
+  test('should call "onPreviousClick"', () => {
+    expect(props.onPreviousClick).toHaveBeenCalled();
+  });
+
+  describe('when clicking the next button', () => {
+    beforeEach(() => {
+      userEvent.click(getNextButton());
+    });
+    test('should call "onNextClick"', () => {
+      expect(props.onNextClick).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('when in right-to-left direction', () => {
+  beforeEach(() => {
+    setup(props, { ...dayPickerProps, dir: 'rtl' });
+  });
+  test('the previous button should display the right icon', () => {
+    const icons = root.getElementsByTagName('svg');
+    expect(icons[0]).toHaveTextContent('IconRight');
+  });
+  test('the next button should display the left icon', () => {
+    const icons = root.getElementsByTagName('svg');
+    expect(icons[1]).toHaveTextContent('IconLeft');
+  });
+
+  describe('when clicking the previous button', () => {
+    beforeEach(() => {
+      userEvent.click(getPrevButton());
+    });
+    test('should call "onPreviousClick"', () => {
+      expect(props.onPreviousClick).toHaveBeenCalled();
+    });
+  });
+  describe('when clicking the next button', () => {
+    beforeEach(() => {
+      userEvent.click(getNextButton());
+    });
+    test('should call "onNextClick"', () => {
+      expect(props.onNextClick).toHaveBeenCalled();
+    });
+  });
+});
+
+describe('when the previous month is undefined', () => {
+  beforeEach(() => {
+    setup({ ...props, previousMonth: undefined }, dayPickerProps);
+  });
+  test('the previous button should be disabled', () => {
+    expect(getPrevButton()).toBeDisabled();
+  });
+  test('the next button should be enabled', () => {
+    expect(getNextButton()).toBeEnabled();
+  });
+});
+
+describe('when the next month is undefined', () => {
+  beforeEach(() => {
+    setup({ ...props, nextMonth: undefined }, dayPickerProps);
+  });
+  test('the previous button should be enabled', () => {
+    expect(getPrevButton()).toBeEnabled();
+  });
+  test('the next button should be disabled', () => {
+    expect(getNextButton()).toBeDisabled();
+  });
+});
