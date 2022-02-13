7f1d7aea4591cf4528fc8a5ec9cdc252d81555d4
gpbl <io@gpbl.dev>
2022-02-13 07:00:34 -0500

    docs: include css in example

--- website/examples/start.tsx
+++ website/examples/start.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 
 export default function Example() {
   const [selected, setSelected] = React.useState<Date>();
