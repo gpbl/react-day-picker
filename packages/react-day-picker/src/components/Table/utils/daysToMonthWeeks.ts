c6468742af009aac6aac80c95bf1127da525e064
gpbl <io@gpbl.dev>
2022-02-26 06:08:01 -0500

    Cleanup

--- packages/react-day-picker/src/components/Table/utils/daysToMonthWeeks.ts
+++ packages/react-day-picker/src/components/Table/utils/daysToMonthWeeks.ts
@@ -1,8 +1,13 @@
-import { addDays, differenceInCalendarDays, getWeek, Locale } from 'date-fns';
+import {
+  addDays,
+  differenceInCalendarDays,
+  endOfWeek,
+  getWeek,
+  Locale,
+  startOfWeek
+} from 'date-fns';
 
 import { MonthWeek } from './getMonthWeeks';
-import { getOutsideEndDays } from './getOutsideEndDays';
-import { getOutsideStartDays } from './getOutsideStartDays';
 
 /** Return the weeks including the given days.  */
 export function daysToMonthWeeks(
@@ -14,10 +19,12 @@ export function daysToMonthWeeks(
     firstWeekContainsDate?: 1 | 2 | 3 | 4 | 5 | 6 | 7;
   }
 ): MonthWeek[] {
-  const nOfDays = differenceInCalendarDays(laterDate, earlierDate);
+  const dateLeft = endOfWeek(laterDate, options);
+  const dateRight = startOfWeek(earlierDate, options);
+  const nOfDays = differenceInCalendarDays(dateLeft, dateRight);
   const days: Date[] = [];
   for (let i = 0; i <= nOfDays; i++) {
-    days.push(addDays(earlierDate, i));
+    days.push(addDays(dateRight, i));
   }
 
   const weeksInMonth = days.reduce((result: MonthWeek[], date) => {
@@ -30,10 +37,10 @@ export function daysToMonthWeeks(
       return result;
     }
     // Add start days to the first week
-    const startDays = getOutsideStartDays(date, options);
+    // const startDays = getOutsideStartDays(date, options);
     result.push({
       weekNumber,
-      dates: [...startDays, date]
+      dates: [date]
     });
     return result;
   }, []);
@@ -43,10 +50,10 @@ export function daysToMonthWeeks(
     throw new Error('No weeks found between the given dates');
   }
 
-  const lastWeek = weeksInMonth[weeksInMonth.length - 1];
-  const lastDay = lastWeek.dates[lastWeek.dates.length - 1];
-  const endDays = getOutsideEndDays(lastDay, options);
-  lastWeek.dates = [...lastWeek.dates, ...endDays];
+  // const lastWeek = weeksInMonth[weeksInMonth.length - 1];
+  // const lastDay = lastWeek.dates[lastWeek.dates.length - 1];
+  // const endDays = getOutsideEndDays(lastDay, options);
+  // lastWeek.dates = [...lastWeek.dates, ...endDays];
 
   return weeksInMonth;
 }
--- packages/react-day-picker/src/components/Table/utils/getMonthWeeks.ts
+++ packages/react-day-picker/src/components/Table/utils/getMonthWeeks.ts
@@ -1,7 +1,5 @@
 import {
-  addDays,
   addWeeks,
-  differenceInCalendarDays,
   endOfMonth,
   getWeeksInMonth,
   Locale,
