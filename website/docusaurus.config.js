693864b4b1612c67a027e2c81bea04eb77a5c262
gpbl <io@gpbl.dev>
2022-02-27 05:06:31 -0500

    chore(website): remove breadcrumbs
    This setting was introduced in docusaurus 16.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,9 +1,21 @@
+// @ts-check */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const typedocConfig = require('./docusaurus.typedoc');
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+
+/** @type {import('@docusaurus/preset-classic').Options} */
+const presetClassicConfig = {
+  docs: {
+    sidebarPath: require.resolve('./docusaurus.sidebars.js'),
+    routeBasePath: '/',
+    breadcrumbs: false
+  },
+  gtag: { trackingID: 'UA-68185118-1' },
+  theme: { customCss: require.resolve('./src/custom.css') }
+};
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -19,24 +31,13 @@ const config = {
     require.resolve('@codesandbox/sandpack-react/dist/index.css')
   ],
   themeConfig: {
+    hideableSidebar: false,
     image: 'images/favicon.png',
     navbar: require('./docusaurus.navbar.js'),
     editUrl: 'https://github.com/gpbl/react-day-picker/edit/master/website/',
     prism: { theme: lightCodeTheme, darkTheme: darkCodeTheme }
   },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./docusaurus.sidebars.js'),
-          routeBasePath: '/'
-        },
-        gtag: { trackingID: 'UA-68185118-1' },
-        theme: { customCss: require.resolve('./src/custom.css') }
-      }
-    ]
-  ],
+  presets: [['@docusaurus/preset-classic', presetClassicConfig]],
   plugins: [['docusaurus-plugin-typedoc', typedocConfig]]
 };
 
