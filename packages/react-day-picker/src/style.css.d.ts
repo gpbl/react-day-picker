5b5b68c1f26ed0f26ddc27d90d95852574a2d867
gpbl <io@gpbl.dev>
2022-03-23 20:12:36 -0500

    fix: foot selector typo

--- packages/react-day-picker/src/style.css.d.ts
+++ packages/react-day-picker/src/style.css.d.ts
@@ -24,7 +24,7 @@ declare const styles: {
   readonly 'rdp-row': string;
   readonly 'rdp-head_cell': string;
   readonly 'rdp-tbody': string;
-  readonly 'rdp-foot': string;
+  readonly 'rdp-tfoot': string;
   readonly 'rdp-cell': string;
   readonly 'rdp-weeknumber': string;
   readonly 'rdp-day': string;
