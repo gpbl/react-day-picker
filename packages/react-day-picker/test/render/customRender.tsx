fa2246ab72ac40cd65c04b90421c52c8ce3a39f2
gpbl <io@gpbl.dev>
2022-03-25 15:01:12 -0500

    Update prop

--- packages/react-day-picker/test/render/customRender.tsx
+++ packages/react-day-picker/test/render/customRender.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+import { DayPickerProps } from 'DayPicker';
 
 import { RootProvider } from 'contexts/RootProvider';
-import { DayPickerBase } from 'types/DayPickerBase';
 
 /** Render a React Element wrapped with the Root Provider. */
 export function customRender(
   /** The element to render. */
   element: React.ReactElement,
   /** The initial DayPicker props to pass to the Root Provider. */
-  dayPickerProps: DayPickerBase = {}
+  dayPickerProps: DayPickerProps = {}
 ) {
   return render(<RootProvider {...dayPickerProps}>{element}</RootProvider>);
 }
