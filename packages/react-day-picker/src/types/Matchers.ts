60611a0555bee7b1cc63e42faa6d7b965c3993ae
gpbl <io@gpbl.dev>
2022-04-03 09:18:18 -0500

    chore: clean up comments

--- packages/react-day-picker/src/types/Matchers.ts
+++ packages/react-day-picker/src/types/Matchers.ts
@@ -87,7 +87,6 @@ export function isDateInterval(matcher: unknown): matcher is DateInterval {
 
 /** Returns true if `value` is a [[DateRange]] type. */
 export function isDateRange(value: unknown): value is DateRange {
-  // TODO: Check if dates?!
   return Boolean(value && typeof value === 'object' && 'from' in value);
 }
 
