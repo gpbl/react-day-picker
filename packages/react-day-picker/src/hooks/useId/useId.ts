20df70e3360ec41405218fa1acc77614b6b193e4
Giampaolo Bellavite <gbellavite@thefarmersdog.com>
2022-07-17 18:13:39 -0500

    Include extra modules

--- packages/react-day-picker/src/hooks/useId/useId.ts
+++ packages/react-day-picker/src/hooks/useId/useId.ts
@@ -69,7 +69,40 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 import * as React from 'react';
 
-import { useIsomorphicLayoutEffect as useLayoutEffect } from '@reach/utils';
+function canUseDOM() {
+  return !!(
+    typeof window !== 'undefined' &&
+    window.document &&
+    window.document.createElement
+  );
+}
+/**
+ * React currently throws a warning when using useLayoutEffect on the server. To
+ * get around it, we can conditionally useEffect on the server (no-op) and
+ * useLayoutEffect in the browser. We occasionally need useLayoutEffect to
+ * ensure we don't get a render flash for certain operations, but we may also
+ * need affected components to render on the server. One example is when setting
+ * a component's descendants to retrieve their index values.
+ *
+ * Important to note that using this hook as an escape hatch will break the
+ * eslint dependency warnings unless you rename the import to `useLayoutEffect`.
+ * Use sparingly only when the effect won't effect the rendered HTML to avoid
+ * any server/client mismatch.
+ *
+ * If a useLayoutEffect is needed and the result would create a mismatch, it's
+ * likely that the component in question shouldn't be rendered on the server at
+ * all, so a better approach would be to lazily render those in a parent
+ * component after client-side hydration.
+ *
+ * https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
+ * https://github.com/reduxjs/react-redux/blob/master/src/utils/useIsomorphicLayoutEffect.js
+ *
+ * @param effect
+ * @param deps
+ */
+const useIsomorphicLayoutEffect = canUseDOM()
+  ? React.useLayoutEffect
+  : React.useEffect;
 
 let serverHandoffComplete = false;
 let id = 0;
@@ -109,7 +142,7 @@ function useId(providedId?: number | string | undefined | null) {
   let initialId = providedId ?? (serverHandoffComplete ? genId() : null);
   let [id, setId] = React.useState(initialId);
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     if (id === null) {
       // Patch the ID after render. We do this in `useLayoutEffect` to avoid any
       // rendering flicker, though it'll make the first render slower (unlikely
