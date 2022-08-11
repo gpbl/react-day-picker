ffb69e94fb750940bd1e7a2c6885381273917732
gpbl <io@gpbl.dev>
2022-08-11 12:09:01 -0500

    website: improve layout for the examples (#1498)

--- website/src/components/RenderExample.tsx
+++ website/src/components/RenderExample.tsx
@@ -1,15 +1,34 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
 
-/** Renders an example component from the examples directory.  */
-export function RenderExample(props: { fileName: string }) {
-  try {
-    require(`../../examples/${props.fileName}`).default;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error('Error requiring %s', `../../examples/${props.fileName}`, e);
-    return <pre>{e.message}</pre>;
-  }
-  const Component = require(`../../examples/${props.fileName}`).default;
-  return <Component />;
+import BrowserOnly from '@docusaurus/BrowserOnly';
+import root from 'react-shadow';
+
+export function RenderExample(props: {
+  name: string;
+  rootStyle?: React.CSSProperties;
+}) {
+  return (
+    <BrowserOnly>
+      {() => {
+        try {
+          require(`@site/examples/${props.name}`).default;
+        } catch (e) {
+          return <pre>{e.message}</pre>;
+        }
+        const Component = require(`@site/examples/${props.name}`).default;
+        const libStyle =
+          require(`!raw-loader!react-day-picker/dist/style.css`).default;
+        const style = require(`!raw-loader!./shadow-dom-styles.css`).default;
+
+        return (
+          <root.div style={props.rootStyle}>
+            <style type="text/css">{libStyle}</style>
+            <style type="text/css">{style}</style>
+            <Component />
+          </root.div>
+        );
+      }}
+    </BrowserOnly>
+  );
 }
