1658c9f0623471d4ba213fe6b7651f6d84f2a011
gpbl <io@gpbl.dev>
2022-08-11 18:44:17 -0500

    website: fix dark theme after CSS variables change

--- website/src/components/RenderExample.tsx
+++ website/src/components/RenderExample.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 
 import BrowserOnly from '@docusaurus/BrowserOnly';
+import { useColorMode } from '@docusaurus/theme-common';
 import root from 'react-shadow';
 
 export function RenderExample(props: {
   name: string;
   rootStyle?: React.CSSProperties;
 }) {
+  const isDarkTheme = useColorMode().colorMode === 'dark';
   return (
     <BrowserOnly>
       {() => {
@@ -20,11 +22,14 @@ export function RenderExample(props: {
         const libStyle =
           require(`!raw-loader!react-day-picker/dist/style.css`).default;
         const style = require(`!raw-loader!./shadow-dom-styles.css`).default;
+        const styleDark =
+          require(`!raw-loader!./shadow-dom-styles-dark.css`).default;
 
         return (
           <root.div style={props.rootStyle}>
             <style type="text/css">{libStyle}</style>
             <style type="text/css">{style}</style>
+            <style type="text/css">{isDarkTheme && styleDark}</style>
             <Component />
           </root.div>
         );
