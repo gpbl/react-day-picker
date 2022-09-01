404ccaf253e8dab376a5257143d5f10d0eff05b2
gpbl <io@gpbl.dev>
2022-09-01 09:54:36 -0500

    new: custom HeadRow component (#1534)

--- packages/react-day-picker/src/components/Head/Head.tsx
+++ packages/react-day-picker/src/components/Head/Head.tsx
@@ -5,11 +5,11 @@ import { useDayPicker } from 'contexts/DayPicker';
 
 /** Render the table head. */
 export function Head(): JSX.Element {
-  const { classNames, styles } = useDayPicker();
-
+  const { classNames, styles, components } = useDayPicker();
+  const HeadRowComponent = components?.HeadRow ?? HeadRow;
   return (
     <thead style={styles.head} className={classNames.head}>
-      <HeadRow />
+      <HeadRowComponent />
     </thead>
   );
 }
