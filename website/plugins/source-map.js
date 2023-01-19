e24cf6448d90d94bdbc61c926014c882b717a284
gpbl <io@gpbl.dev>
2023-01-19 09:35:53 -0500

    website(chore): add source maps (#1652)

---
+++ website/plugins/source-map.js
@@ -0,0 +1,28 @@
+// @ts-check
+/* eslint-env node */
+/**
+ * Docusaurus plugin to add https://webpack.js.org/loaders/source-map-loader so
+ * that we can have corgi-x source maps in corgi-website for better debugging.
+ * @type {import('@docusaurus/types').PluginModule}
+ */
+const sourceMapPlugin = () => {
+  return {
+    name: 'docusaurus-source-map-plugin',
+    configureWebpack() {
+      return {
+        devtool: 'source-map',
+        ignoreWarnings: [/Failed to parse source map/],
+        module: {
+          rules: [
+            {
+              test: /\.(jsx|js|ts|tsx)$/,
+              enforce: 'pre',
+              use: ['source-map-loader'],
+            },
+          ],
+        },
+      };
+    },
+  };
+};
+module.exports = sourceMapPlugin;
