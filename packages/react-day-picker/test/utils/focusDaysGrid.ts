abdbf9be511e9aa5dd8934f0826fd14f23a61da4
gpbl <io@gpbl.dev>
2023-04-21 18:33:23 -0500

    build(deps): Upgrade dependencies (#1768)

--- packages/react-day-picker/test/utils/focusDaysGrid.ts
+++ packages/react-day-picker/test/utils/focusDaysGrid.ts
@@ -1,10 +1,11 @@
+import userEvent from '@testing-library/user-event';
 import { fireEvent } from '@testing-library/dom';
 import { act } from '@testing-library/react';
-import { UserEvent } from '@testing-library/user-event/dist/types/setup/setup';
 
 import { getFocusedElement } from '../selectors';
+const user = userEvent.setup();
 
-export async function focusDaysGrid(user: UserEvent) {
+export async function focusDaysGrid() {
   // Make sure nothing is focused
   await act(() => fireEvent.blur(getFocusedElement()));
   // By pressing tab 3 times
