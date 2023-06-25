cd21ff99d55a24e699fd5f0f9b19e6d831edae8a
gpbl <io@gpbl.dev>
2023-06-25 18:41:05 -0500

    Update examples

--- website/src/components/RenderExample.tsx
+++ website/src/components/RenderExample.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import styles from './RenderExample.module.css';
+
 export function RenderExample(props: {
   name: string;
   rootStyle?: React.CSSProperties;
 }) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const Component = require(`@site/examples/${props.name}`).default;
-  return <Component />;
+  return (
+    <div className={styles.example}>
+      <Component />
+    </div>
+  );
 }
