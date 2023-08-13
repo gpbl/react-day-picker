f1f38aa76f61f00535f16f3745eb4d8735ed63ef
Travis Vallon <travisnormanv@gmail.com>
2023-08-14 00:37:37 +0900

    Add flag to disable selecting days range with disabled days in between.

--- src/types/DayPickerBase.ts
+++ src/types/DayPickerBase.ts
@@ -287,6 +287,12 @@ export interface DayPickerBase {
    */
   formatters?: Partial<Formatters>;
 
+  /**
+   * Disables other dates that cannot be consecutively selected within a range
+   * of non-disabled dates.
+   */
+  disableNonConsecutiveDates?: boolean;
+
   onDayClick?: DayClickEventHandler;
   onDayFocus?: DayFocusEventHandler;
   onDayBlur?: DayFocusEventHandler;
