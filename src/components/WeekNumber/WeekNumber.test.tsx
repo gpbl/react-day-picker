8fe70aec1ea151e75e9c6fdfca0c211ac3ff2de3
gpbl <io@gpbl.dev>
2023-09-25 07:01:15 -0500

    Lint files

--- src/components/WeekNumber/WeekNumber.test.tsx
+++ src/components/WeekNumber/WeekNumber.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 import { DayPickerProps } from 'DayPicker';
 
 import { customRender } from 'test/render/customRender';
