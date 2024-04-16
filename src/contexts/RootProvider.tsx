7dcdd95924ec32b37443391b88dda58bf18daab7
union-zakbutcher <zak.butcher@getunion.com>
2024-04-15 20:43:26 -0400

    fix(types): Add support for props that exist on downstream providers whose props are derived from prop spreading (#2073)
    Add support for props that exist on downstream providers whose props are derrived from prop spreading

--- src/contexts/RootProvider.tsx
+++ src/contexts/RootProvider.tsx
@@ -8,11 +8,21 @@ import { NavigationProvider } from './Navigation';
 import { SelectMultipleProvider } from './SelectMultiple';
 import { SelectRangeProvider } from './SelectRange';
 import { SelectSingleProvider } from './SelectSingle';
+import { DayPickerDefaultProps } from 'types/DayPickerDefault';
+import { DayPickerSingleProps } from 'types/DayPickerSingle';
+import { DayPickerMultipleProps } from 'types/DayPickerMultiple';
+import { DayPickerRangeProps } from 'types/DayPickerRange';
+
+type RootContextProps =
+  | Partial<DayPickerDefaultProps>
+  | Partial<DayPickerSingleProps>
+  | Partial<DayPickerMultipleProps>
+  | Partial<DayPickerRangeProps>;
 
 /** The props of {@link RootProvider}. */
-export interface RootContext {
+export type RootContext = RootContextProps & {
   children?: ReactNode;
-}
+};
 
 /** Provide the value for all the context providers. */
 export function RootProvider(props: RootContext): JSX.Element {
