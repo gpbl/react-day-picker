bb64279764a19d191a1a665803b901db1c6e8bcb
gpbl <io@gpbl.dev>
2024-05-08 17:17:45 -0600

    Update jsdoc

--- test/render/customRender.tsx
+++ test/render/customRender.tsx
@@ -5,12 +5,14 @@ import { RenderResult, render } from "@testing-library/react";
 import { DayPickerProps } from "../../src/DayPicker";
 import { ContextProviders } from "../../src/contexts/ContextProviders";
 
-/** Render a React Element wrapped with the Root Provider. */
+/** Render a React Element wrapped with the context providers. */
 export function customRender(
   /** The element to render. */
   element: ReactElement,
-  /** The initial DayPicker props to pass to the Root Provider. */
+  /** The initial DayPicker props to pass to the context providers. */
   dayPickerProps: DayPickerProps = {}
 ): RenderResult {
-  return render(<ContextProviders {...dayPickerProps}>{element}</ContextProviders>);
+  return render(
+    <ContextProviders {...dayPickerProps}>{element}</ContextProviders>
+  );
 }
