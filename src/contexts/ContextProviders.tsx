ae2681c996e03a7c54912f0aa0fce9420337a3e6
gpbl <io@gpbl.dev>
2024-05-12 05:18:48 -0500

    Lint file

--- src/contexts/ContextProviders.tsx
+++ src/contexts/ContextProviders.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode } from "react";
+import { PropsWithChildren } from "react";
 
 import { PropsDefault } from "../types/PropsDefault";
 import { PropsMulti } from "../types/PropsMulti";
