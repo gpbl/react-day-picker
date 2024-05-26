efbee7b67ab4f2de05c355dee8a318e30dbc5866
Giampaolo Bellavite <io@gpbl.dev>
2024-05-26 07:09:43 -0500

    Fix missing nav

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -66,7 +66,7 @@ export function Calendar() {
       title={title}
       {...dataAttributes}
     >
-      {dropdownNavigation && !hideNavigation && <Nav />}
+      {!hideNavigation && <Nav />}
       <Months
         className={classNames.months_wrapper}
         style={styles?.months_wrapper}
