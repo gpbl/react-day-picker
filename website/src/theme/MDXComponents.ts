d471e88b8a5a625424ef92c85775a8232eb39697
Giampaolo Bellavite <io@gpbl.dev>
2024-05-27 19:17:49 -0500

    Cleanup file

--- website/src/theme/MDXComponents.ts
+++ website/src/theme/MDXComponents.ts
@@ -6,10 +6,7 @@ import * as Examples from "../../examples-v9";
 import { BrowserWindow } from "../components/BrowserWindow";
 
 export default {
-  // Re-use the default mapping
   ...MDXComponents,
-  // Map the "<Highlight>" tag to our Highlight component
-  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   BrowserWindow,
   Examples,
   ExamplesV8
