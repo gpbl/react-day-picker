4a8803b7e460e0adfd85e7adbc090c7383c8ddf3
Giampaolo Bellavite <io@gpbl.dev>
2024-05-29 07:46:47 -0500

    build: fix css types

--- src/style.d.ts
+++ src/style.d.ts
@@ -1,9 +1,8 @@
-import { UI } from "./types";
+import { UI } from "./UI";
 
 /** The class names that can passed to DayPicker's `classNames` prop. */
-const classNames: {
+declare const classNames: {
   [uiElement in UI]: string;
-// eslint-disable-next-line prettier/prettier
-}
+};
 
 export default classNames;
