011bc62a2fbb159fdab3c2a8a8b1b79a8870fd2f
Giampaolo Bellavite <io@gpbl.dev>
2024-05-31 06:34:00 -0500

    website: fix import path

--- website/src/theme/MDXComponents.ts
+++ website/src/theme/MDXComponents.ts
@@ -2,7 +2,7 @@
 import MDXComponents from "@theme-original/MDXComponents";
 import * as Examples from "react-day-picker/examples";
 
-import * as ExamplesV8 from "../../examples";
+import * as ExamplesV8 from "../../../examples";
 import { BrowserWindow } from "../components/BrowserWindow";
 
 export default {
