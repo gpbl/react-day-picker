cc930c3cd6a75c5168fbda35b74e20072697a9bd
Giampaolo Bellavite <io@gpbl.dev>
2024-06-03 16:56:36 -0500

    docs: add comments to CalendarDay

--- src/classes/CalendarDay.ts
+++ src/classes/CalendarDay.ts
@@ -1,7 +1,13 @@
 import { isSameDay } from "date-fns/isSameDay";
 import { isSameMonth } from "date-fns/isSameMonth";
 
-/** Represent the day displayed in the Day grid cell. */
+/**
+ * Represent the day displayed in the calendar.
+ *
+ * In DayPicker, a `Day` is a `Date` that can be displayed in the calendar. It
+ * is used as extension of the native `Date` object to provide additional
+ * information about the day.
+ */
 export class CalendarDay {
   constructor(date: Date, displayMonth: Date) {
     this.date = date;
@@ -9,10 +15,21 @@ export class CalendarDay {
     this.outside = Boolean(displayMonth && !isSameMonth(date, displayMonth));
   }
 
-  /** Whether the day is outside the display month. */
+  /**
+   * Whether the day is not belonging to the displayed month.
+   *
+   * When `outside` is `true`, use `displayMonth` to know to which month the day
+   * belongs.
+   */
   readonly outside: boolean;
 
-  /** The months where the day is displayed. */
+  /**
+   * The months where the day is displayed.
+   *
+   * In DayPicker, days can fall out the displayed months (e.g. when
+   * `showOutsideDays` is `true`). This property is useful to know if the day is
+   * in the same month of the displayed month.
+   */
   readonly displayMonth: Date;
 
   /** The date represented by this day. */
