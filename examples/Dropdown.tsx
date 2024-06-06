89197a2d082480e2c44fe05f2a6a56b9e4f49ada
Giampaolo Bellavite <io@gpbl.dev>
2024-06-05 19:21:14 -0500

    Update caption layout example

--- examples/Dropdown.tsx
+++ examples/Dropdown.tsx
@@ -3,5 +3,11 @@ import React from "react";
 import { DayPicker } from "react-day-picker";
 
 export function Dropdown() {
-  return <DayPicker captionLayout="dropdown" fromYear={2010} />;
+  return (
+    <DayPicker
+      captionLayout="dropdown"
+      fromMonth={new Date(2015, 6)}
+      toMonth={new Date(2025, 9)}
+    />
+  );
 }
