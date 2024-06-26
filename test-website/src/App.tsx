30a4105b2871c16f921cf1317fc786a88b45efdf
Giampaolo Bellavite <io@gpbl.dev>
2024-06-25 19:12:19 -0500

    repo: add a test website

---
+++ test-website/src/App.tsx
@@ -0,0 +1,14 @@
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <DayPicker mode="single" />
+    </>
+  );
+}
+
+export default App;
