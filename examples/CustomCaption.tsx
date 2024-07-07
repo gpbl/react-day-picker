1adcad8c673eb6b5370a4eaabc44112b0159540b
Giampaolo Bellavite <io@gpbl.dev>
2024-07-07 08:04:30 -0500

    docs: update CustomCaption example

--- examples/CustomCaption.tsx
+++ examples/CustomCaption.tsx
@@ -10,27 +10,32 @@ import {
 function CustomMonthCaption(props: MonthCaptionProps) {
   const { goToMonth, nextMonth, previousMonth } = useDayPicker();
   return (
-    <h2>
-      {format(props.calendarMonth.date, "MMM yyy")}
-      <button
-        disabled={!previousMonth}
-        onClick={() => previousMonth && goToMonth(previousMonth)}
-      >
-        Previous
-      </button>
-      <button
-        disabled={!nextMonth}
-        onClick={() => nextMonth && goToMonth(nextMonth)}
-      >
-        Next
-      </button>
-    </h2>
+    <>
+      <h2>{format(props.calendarMonth.date, "MMM yyy")}</h2>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <button
+          style={{ all: "revert", cursor: "pointer" }}
+          disabled={!previousMonth}
+          onClick={() => previousMonth && goToMonth(previousMonth)}
+        >
+          Previous
+        </button>
+        <button
+          style={{ all: "revert", cursor: "pointer" }}
+          disabled={!nextMonth}
+          onClick={() => nextMonth && goToMonth(nextMonth)}
+        >
+          Next
+        </button>
+      </div>
+    </>
   );
 }
 
 export function CustomCaption() {
   return (
     <DayPicker
+      hideNavigation
       components={{
         MonthCaption: CustomMonthCaption
       }}
