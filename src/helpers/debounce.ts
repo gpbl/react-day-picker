973a4aadc40553035ab6a7f794cdcee3106bc06a
Giampaolo Bellavite <io@gpbl.dev>
2024-07-06 19:59:30 -0500

    chore: remove unused function

--- src/helpers/debounce.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function debounce(func: Function, wait: number) {
-  let timeout: NodeJS.Timeout;
-
-  return (...args: unknown[]) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), wait);
-  };
-}
