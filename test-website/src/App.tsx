aca2a5574c317b1330a0d26043828feac0f9f240
Giampaolo Bellavite <io@gpbl.dev>
2024-07-12 19:01:43 -0500

    docs(website): add example to querystring

--- test-website/src/App.tsx
+++ test-website/src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import * as Examples from "react-day-picker/examples";
 import "react-day-picker/style.css";
 
 import "./App.css";
 
+const updateQueryString = (key: string, value: string) => {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  params.set(key, value);
+  window.history.pushState({}, "", `${url.pathname}?${params.toString()}`);
+};
+
 function App() {
-  const [example, setExample] = React.useState("Start");
+  const [example, setExample] = useState("Start");
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const value = params.get("example");
+    if (value) {
+      setExample(value);
+    }
+  }, []);
 
   const Component: React.ComponentType = Examples[
     example as keyof typeof Examples
@@ -16,7 +31,14 @@ function App() {
       {/* create a select that allows to choose between the examples */}
       <label>
         Example:{" "}
-        <select onChange={(e) => setExample(e.target.value)} value={example}>
+        <select
+          name="example_name"
+          onChange={(e) => {
+            updateQueryString("example", e.target.value);
+            setExample(e.target.value);
+          }}
+          value={example}
+        >
           {Object.keys(Examples).map((name) => (
             <option key={name} value={name}>
               {name}
