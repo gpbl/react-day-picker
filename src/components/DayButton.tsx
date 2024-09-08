9fb76f6b7708d518cd2f5522ef831fb9cb222230
Giampaolo Bellavite <io@gpbl.dev>
2024-09-08 18:43:08 -0500

    docs: update DayButton component jsdocs

--- src/components/DayButton.tsx
+++ src/components/DayButton.tsx
@@ -6,9 +6,6 @@ import type { Modifiers } from "../types/index.js";
 /**
  * Render the button for a day in the calendar.
  *
- * When not interactive, DayPicker will render a `DayContent` component instead
- * of a `DayButton` component.
- *
  * @group Components
  * @see https://daypicker.dev/guides/custom-components
  */
