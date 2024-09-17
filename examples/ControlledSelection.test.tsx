0058edf05d88ba090a6406afd7d1e65425d3b335
Giampaolo Bellavite <io@gpbl.dev>
2024-09-17 18:50:21 -0500

    Improve example and add test

---
+++ examples/ControlledSelection.test.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { dateButton, gridcell } from "@/test/elements";
+import { render } from "@/test/render";
+import { user } from "@/test/user";
+
+import { ControlledSelection } from "./ControlledSelection";
+
+const today = new Date(2024, 8, 17);
+beforeAll(() => jest.setSystemTime(today));
+afterAll(() => jest.useRealTimers());
+
+beforeEach(async () => {});
+
+test("a range is selected following the", async () => {
+  render(<ControlledSelection />);
+  await user.click(dateButton(new Date(2024, 8, 1)));
+  await user.click(dateButton(new Date(2024, 8, 4)));
+
+  expect(gridcell(new Date(2024, 8, 1), true)).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 2), true)).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 3), true)).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 4), true)).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 5), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+});
+
+test("the selected range is reset on third click", async () => {
+  render(<ControlledSelection />);
+  await user.click(dateButton(new Date(2024, 8, 1)));
+  await user.click(dateButton(new Date(2024, 8, 4)));
+  await user.click(dateButton(new Date(2024, 8, 5)));
+
+  expect(gridcell(new Date(2024, 8, 1), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 2), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 3), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 4), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 5), true)).toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+  expect(gridcell(new Date(2024, 8, 6), true)).not.toHaveAttribute(
+    "aria-selected",
+    "true"
+  );
+});
--- examples/ControlledSelection.tsx
+++ examples/ControlledSelection.tsx
@@ -6,13 +6,13 @@ import { DateRange, DayPicker } from "react-day-picker";
 export function ControlledSelection() {
   const [selected, setSelected] = useState<DateRange | undefined>();
 
-  function handleOnSelect(range: DateRange, triggerDate: Date) {
+  function handleOnSelect(range: DateRange | undefined, triggerDate: Date) {
+    // Change the behavior of the selection when a range is already selected
     if (selected?.from && selected?.to) {
-      setSelected({
+      return setSelected({
         from: triggerDate,
         to: undefined
       });
-      return;
     }
     setSelected(range);
   }
@@ -20,7 +20,7 @@ export function ControlledSelection() {
   return (
     <DayPicker
       mode="range"
-      required
+      min={1}
       selected={selected}
       onSelect={handleOnSelect}
     />
