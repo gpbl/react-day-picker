067f25bcda27944012d0162b0f50fc44f02369e4
Giampaolo Bellavite <io@gpbl.dev>
2024-09-17 19:07:00 -0500

    Update for the jalali library

--- examples/Jalali.tsx
+++ examples/Jalali.tsx
@@ -4,5 +4,5 @@ import { faIR } from "date-fns/locale";
 import { DayPicker } from "react-day-picker/jalali";
 
 export function Jalali() {
-  return <DayPicker mode="single" locale={faIR} dir="rtl" />;
+  return <DayPicker mode="range" locale={faIR} dir="rtl" />;
 }
