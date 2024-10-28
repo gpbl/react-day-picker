da1ae9bb23ec8451d46fa2c10a05d1ac163245a5
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2024-10-28 21:42:06 +0100

    Fix dateMatchModifiers docs

--- src/utils/dateMatchModifiers.ts
+++ src/utils/dateMatchModifiers.ts
@@ -18,14 +18,14 @@ import {
  * ```tsx
  * const date = new Date(2022, 5, 19);
  * const matcher1: DateRange = {
- *  from: new Date(2021, 12, 21),
- *  to: new Date(2021, 12, 30)
- * }
+ *   from: new Date(2021, 12, 21),
+ *   to: new Date(2021, 12, 30)
+ * };
  * const matcher2: DateRange = {
- *  from: new Date(2022, 5, 1),
- *  to: new Date(2022, 5, 23)
- * }
- * const dateMatchModifiers(date, [matcher1, matcher2]); // true, since day is in the matcher1 range.
+ *   from: new Date(2022, 5, 1),
+ *   to: new Date(2022, 5, 23)
+ * };
+ * dateMatchModifiers(date, [matcher1, matcher2]); // true, since day is in the matcher1 range.
  * ```
  *
  * @group Utilities
