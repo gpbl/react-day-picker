f4c4bbc5355933f705a33e3b39777eadea467388
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2024-10-28 21:57:55 +0100

    Improve long range examples

--- examples/RangeLongExcludeDisabled.tsx
+++ examples/RangeLongExcludeDisabled.tsx
@@ -4,7 +4,7 @@ import { type DateRange, DayPicker } from "react-day-picker";
 
 export function RangeLongExcludeDisabled() {
   const [range, setRange] = useState<DateRange | undefined>({
-    from: new Date(1, 0, 1),
+    from: new Date(100, 0, 1),
     to: new Date(2024, 9, 10)
   });
 
