0f50f3b3a25c337b13d834d44770d2e8d1833a3b
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2024-10-28 19:40:30 +0100

    Rename rangeMatchModifiers to rangeContainsModifiers

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,5 @@ export * from "./addToRange.js";
 export * from "./dateMatchModifiers.js";
 export * from "./rangeContainsDayOfWeek.js";
 export * from "./rangeIncludesDate.js";
-export * from "./rangeMatchModifiers.js";
+export * from "./rangeContainsModifiers.js";
 export * from "./typeguards.js";
