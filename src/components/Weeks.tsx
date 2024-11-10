51e6f7bdd64b2bd9ffacabf35bf6a51b82e7d331
Giampaolo Bellavite <io@gpbl.dev>
2024-11-10 18:14:35 -0500

    Progress

--- src/components/Weeks.tsx
+++ src/components/Weeks.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import { useDayPicker } from "../useDayPicker.js";
 
 /**
  * Render the weeks in the month grid.
@@ -7,7 +11,31 @@ import React from "react";
  * @see https://daypicker.dev/guides/custom-components
  */
 export function Weeks(props: JSX.IntrinsicElements["tbody"]) {
-  return <tbody {...props} />;
+  const [direction, setDirection] = useState<"next" | "prev">("next");
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
+  const dayPicker = useDayPicker();
+
+  const key = dayPicker.months[0].date.toISOString();
+
+  console.log(key);
+  return (
+    <TransitionGroup component={null}>
+      <CSSTransition
+        key={key}
+        timeout={250}
+        classNames={
+          direction === "next"
+            ? "animate rdp-slide-next"
+            : "animate rdp-slide-prev"
+        }
+        onEnter={() => setIsTransitioning(true)}
+        onEntered={() => setIsTransitioning(false)}
+      >
+        <tbody {...props}>{props.children}</tbody>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+  // return <tbody {...props} />;
 }
 
 export type WeeksProps = Parameters<typeof Weeks>[0];
