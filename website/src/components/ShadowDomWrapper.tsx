0cb8b4f5d6691fdbd8c44e02373f219fb974131e
Giampaolo Bellavite <io@gpbl.dev>
2024-11-14 18:13:21 -0500

    docs: use react-shadow to isolate style
    Fix #2580

--- website/src/components/ShadowDomWrapper.tsx
+++ website/src/components/ShadowDomWrapper.tsx
@@ -1,3 +1,23 @@
+import style from "!raw-loader!react-day-picker/style.css";
+import { useColorMode } from "@docusaurus/theme-common";
+import root from "react-shadow";
+
 export function ShadowDomWrapper({ children }: { children: React.ReactNode }) {
-  return children;
+  const colorMode = useColorMode();
+  return (
+    <root.div>
+      {children}
+      <style>{style.toString()}</style>
+      {colorMode.isDarkTheme && (
+        <style>{`
+          .rdp-root {
+              --rdp-accent-color: var(--ifm-color-primary);
+              --rdp-accent-background-color: #073845;
+              --rdp-range_end-color: black;
+              --rdp-range_start-color: black;
+            }
+          `}</style>
+      )}
+    </root.div>
+  );
 }
