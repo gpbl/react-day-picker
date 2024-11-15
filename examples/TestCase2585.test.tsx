3eac3dba5ecafb928c2bdb5bd97e294af724897d
Giampaolo Bellavite <io@gpbl.dev>
2024-11-15 17:57:44 -0500

    Add test case

---
+++ examples/TestCase2585.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { render, screen } from "@/test/render";
+
+import { TestCase2585 } from "./TestCase2585";
+
+render(<TestCase2585 />);
+
+test("should render 42*12 days", () => {
+  expect(screen.getAllByRole("cell")).toHaveLength(42 * 12);
+});
