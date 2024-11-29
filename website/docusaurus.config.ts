82fd69d7e11ad256b00092fa93bbe5ff54304603
gpbl <io@gpbl.dev>
2024-11-29 18:35:59 -0500

    docs: add sitemap to docusaurus

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -53,6 +53,18 @@ const config: Config = {
         blog: false,
         theme: {
           customCss: ["./src/css/custom.css", "../src/style.css"]
+        },
+        sitemap: {
+          lastmod: "date",
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: ["/tags/**"],
+          filename: "sitemap.xml",
+          createSitemapItems: async (params) => {
+            const { defaultCreateSitemapItems, ...rest } = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes("/page/"));
+          }
         }
       } satisfies Preset.Options
     ]
