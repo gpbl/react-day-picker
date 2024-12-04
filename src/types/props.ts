19688b261c794b067c0b77a6b307dc327bbf6718
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2024-12-04 21:02:53 +0100

    docs: update captionLayout docs

--- src/types/props.ts
+++ src/types/props.ts
@@ -209,7 +209,8 @@ export interface PropsBase {
    * - `year`: display only the dropdown for the years
    *
    * **Note:** showing the dropdown will set the start/end months
-   * {@link fromYear} to 100 years ago, and {@link toYear} to the current year.
+   * {@link startMonth} to 100 years ago, and {@link endMonth} to the end of the
+   * current year.
    *
    * @see https://daypicker.dev/docs/customization#caption-layouts
    */
