ca82f97bb7dc05cffb0bd3cb0c66b5e4daf1b38d
gpbl <io@gpbl.dev>
2024-12-15 11:04:49 -0500

    Defaults jalali calendar to `faIR`, rtl dir

--- examples/Jalali.tsx
+++ examples/Jalali.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
-import { faIR } from "date-fns/locale";
 import { DayPicker } from "react-day-picker/jalali";
 
 export function Jalali() {
-  return <DayPicker mode="range" locale={faIR} dir="rtl" />;
+  return <DayPicker mode="range" />;
 }
