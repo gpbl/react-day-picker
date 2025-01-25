9fa2d2642135bd27b9b5b26c2002eb747caecae0
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2025-01-26 00:11:00 +0100

    fix: display calendar in a valid month when month prop is invalid (#2672)

--- src/helpers/getInitialMonth.test.ts
+++ src/helpers/getInitialMonth.test.ts
@@ -1,50 +1,78 @@
-import { addMonths, isSameMonth } from "date-fns";
+import { addMonths, isSameDay, isSameMonth, startOfMonth } from "date-fns";
 
 import { defaultDateLib } from "../classes/DateLib";
 
 import { getInitialMonth } from "./getInitialMonth";
 
-describe("when no endMonth is given", () => {
+it("return start of month", () => {
+  const month = new Date(2010, 11, 12);
+  const initialMonth = getInitialMonth({ month }, defaultDateLib);
+  expect(isSameDay(initialMonth, startOfMonth(month))).toBe(true);
+});
+
+describe("when no startMonth or endMonth is given", () => {
+  const month = new Date(2010, 11, 12);
+  const defaultMonth = new Date(2011, 11, 12);
+  const today = new Date(2012, 11, 12);
   describe("when month is in context", () => {
-    const month = new Date(2010, 11, 12);
     it("return that month", () => {
-      const startMonth = getInitialMonth({ month }, defaultDateLib);
-      expect(isSameMonth(startMonth, month)).toBe(true);
+      const initialMonth = getInitialMonth(
+        { month, defaultMonth, today },
+        defaultDateLib
+      );
+      expect(isSameMonth(initialMonth, month)).toBe(true);
     });
   });
-  describe("when defaultMonth is in context", () => {
-    const defaultMonth = new Date(2010, 11, 12);
+  describe("when defaultMonth is in context and no month is given", () => {
     it("return that month", () => {
-      const startMonth = getInitialMonth({ defaultMonth }, defaultDateLib);
-      expect(isSameMonth(startMonth, defaultMonth)).toBe(true);
+      const initialMonth = getInitialMonth(
+        { defaultMonth, today },
+        defaultDateLib
+      );
+      expect(isSameMonth(initialMonth, defaultMonth)).toBe(true);
     });
   });
   describe("when no month or defaultMonth", () => {
-    const today = new Date(2010, 11, 12);
     it("return the today month", () => {
-      const startMonth = getInitialMonth({ today }, defaultDateLib);
-      expect(isSameMonth(startMonth, today)).toBe(true);
+      const initialMonth = getInitialMonth({ today }, defaultDateLib);
+      expect(isSameMonth(initialMonth, today)).toBe(true);
     });
   });
 });
+
+describe("when startMonth is given and is after the default initial month", () => {
+  it("return the startMonth", () => {
+    const month = new Date(2010, 11, 12);
+    const startMonth = addMonths(month, 1);
+    const initialMonth = getInitialMonth(
+      { month, numberOfMonths: 3, startMonth },
+      defaultDateLib
+    );
+    expect(isSameMonth(initialMonth, startMonth)).toBe(true);
+  });
+});
+
 describe("when endMonth is given", () => {
-  describe("when endMonth is before the default initial date", () => {
+  describe("when endMonth is before the default initial month", () => {
     const month = new Date(2010, 11, 12);
     const endMonth = addMonths(month, -2);
     describe("when the number of month is 1", () => {
       it("return the endMonth", () => {
-        const startMonth = getInitialMonth({ month, endMonth }, defaultDateLib);
-        expect(isSameMonth(startMonth, endMonth)).toBe(true);
+        const initialMonth = getInitialMonth(
+          { month, endMonth },
+          defaultDateLib
+        );
+        expect(isSameMonth(initialMonth, endMonth)).toBe(true);
       });
     });
     describe("when the number of month is 3", () => {
       it("return the endMonth plus the number of months", () => {
-        const startMonth = getInitialMonth(
+        const initialMonth = getInitialMonth(
           { month, numberOfMonths: 3, endMonth },
           defaultDateLib
         );
         const expectedMonth = addMonths(endMonth, -1 * (3 - 1));
-        expect(isSameMonth(startMonth, expectedMonth)).toBe(true);
+        expect(isSameMonth(initialMonth, expectedMonth)).toBe(true);
       });
     });
   });
