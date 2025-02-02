bd4bf34cf825b7313ec0be70985a1ceefc63d3be
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2025-02-02 20:04:27 +0100

    new(examples): add CustomSelect test

---
+++ examples/CustomSelect/CustomSelect.test.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+
+import { grid, monthDropdown, yearDropdown } from "@/test/elements";
+import { user } from "@/test/user";
+
+import { CustomSelect } from "./CustomSelect";
+
+// Mocks for Radix UI
+window.PointerEvent =
+  class PointerEvent extends Event {} as unknown as typeof window.PointerEvent;
+window.HTMLElement.prototype.scrollIntoView = jest.fn();
+window.HTMLElement.prototype.hasPointerCapture = jest.fn();
+window.HTMLElement.prototype.releasePointerCapture = jest.fn();
+
+const today = new Date(2015, 6, 1);
+
+beforeAll(() => jest.setSystemTime(today));
+afterAll(() => jest.useRealTimers());
+
+beforeEach(() => {
+  render(<CustomSelect />);
+});
+
+test("should display the month dropdown", () => {
+  expect(monthDropdown()).toBeInTheDocument();
+});
+
+test("should display the year dropdown", () => {
+  expect(yearDropdown()).toBeInTheDocument();
+});
+
+test("change month", async () => {
+  expect(grid()).toHaveAccessibleName("July 2015");
+
+  await user.click(yearDropdown());
+  await user.click(screen.getByRole("option", { name: "2000" }));
+  await user.click(monthDropdown());
+  await user.click(screen.getByRole("option", { name: "December" }));
+
+  expect(grid()).toHaveAccessibleName("December 2000");
+});
