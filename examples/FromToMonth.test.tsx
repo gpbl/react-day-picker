181850f21ae84491fe56404957b0779b18bbd7e6
gpbl <io@gpbl.dev>
2025-02-16 18:38:43 -0500

    Update tests

--- examples/FromToMonth.test.tsx
+++ examples/FromToMonth.test.tsx
@@ -13,11 +13,11 @@ beforeEach(() => {
 });
 
 test("the previous button should be disabled", () => {
-  expect(previousButton()).toHaveAttribute("disabled");
+  expect(previousButton()).toHaveAttribute("aria-disabled", true);
 });
 
 test("the next button should be enabled", () => {
-  expect(nextButton()).not.toHaveAttribute("disabled");
+  expect(nextButton()).not.toHaveAttribute("aria-disabled", true);
 });
 
 describe("when navigating to the last month", () => {
@@ -31,10 +31,10 @@ describe("when navigating to the last month", () => {
   });
 
   test("the previous button should not be disabled", () => {
-    expect(previousButton()).not.toHaveAttribute("disabled");
+    expect(previousButton()).not.toHaveAttribute("aria-disabled", true);
   });
 
   test("the next button should be disabled", () => {
-    expect(nextButton()).toHaveAttribute("disabled");
+    expect(nextButton()).toHaveAttribute("aria-disabled", true);
   });
 });
