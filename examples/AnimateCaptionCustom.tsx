69b057cd855df8472cd6c2f8d207d292fbca6636
gpbl <io@gpbl.dev>
2025-03-02 15:30:02 -0500

    Breaking the weeks animation

--- examples/AnimateCaptionCustom.tsx
+++ examples/AnimateCaptionCustom.tsx
@@ -12,12 +12,11 @@ export function AnimateCaptionCustom() {
         caption_prev_enter: "animate-caption_prev_enter",
         caption_prev_exit: "animate-caption_prev_exit",
         caption_next_enter: "animate-caption_next_enter",
-        caption_next_exit: "animate-caption_next_exit"
-        // These animations are not working properly
-        // weeks_prev_enter: "animate-weeks_prev_enter",
-        // weeks_prev_exit: "animate-weeks_prev_exit",
-        // weeks_next_enter: "animate-weeks_next_enter",
-        // weeks_next_exit: "animate-week_next_exit"
+        caption_next_exit: "animate-caption_next_exit",
+        weeks_prev_enter: "animate-weeks_prev_enter",
+        weeks_prev_exit: "animate-weeks_prev_exit",
+        weeks_next_enter: "animate-weeks_next_enter",
+        weeks_next_exit: "animate-week_next_exit"
       }}
     />
   );
