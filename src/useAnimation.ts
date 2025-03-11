222757a94435af2bfba692203f96c128a77c9b69
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2025-03-11 22:29:43 +0100

    fix(animation): fix issues when navigating months during animation

--- src/useAnimation.ts
+++ src/useAnimation.ts
@@ -36,46 +36,11 @@ export function useAnimation(
       !enabled ||
       !rootElRef.current ||
       // safety check because the ref can be set to anything by consumers
-      !(rootElRef.current instanceof HTMLElement)
-    ) {
-      return;
-    }
-
-    // get previous root element snapshot before updating the snapshot ref
-    const previousRootElSnapshot = previousRootElSnapshotRef.current;
-
-    // update snapshot for next effect trigger
-    const rootElSnapshot = rootElRef.current.cloneNode(true);
-    if (rootElSnapshot instanceof HTMLElement) {
-      // if this effect is triggered while animating, we need to remove the old month snapshots from the new root snapshot
-      const currentMonthElsSnapshot = [
-        ...(rootElSnapshot.querySelectorAll(`[data-animated-month]`) ?? [])
-      ];
-      currentMonthElsSnapshot.forEach((currentMonthElSnapshot) => {
-        const previousMonthElSnapshot = currentMonthElSnapshot.querySelector(
-          `[data-animated-month]`
-        );
-        if (
-          previousMonthElSnapshot &&
-          currentMonthElSnapshot.contains(previousMonthElSnapshot)
-        ) {
-          currentMonthElSnapshot.removeChild(previousMonthElSnapshot);
-        }
-      });
-
-      previousRootElSnapshotRef.current = rootElSnapshot;
-    } else {
-      previousRootElSnapshotRef.current = null;
-    }
-
-    // validation required for the animation to work as expected
-    if (
+      !(rootElRef.current instanceof HTMLElement) ||
+      // validation required for the animation to work as expected
       months.length === 0 ||
       previousMonths.length === 0 ||
-      months.length !== previousMonths.length ||
-      // skip animation if a day is focused because it can cause issues to the animation and is better for a11y
-      focused ||
-      animatingRef.current
+      months.length !== previousMonths.length
     ) {
       return;
     }
@@ -85,7 +50,69 @@ export function useAnimation(
       previousMonths[0].date
     );
 
-    if (isSameMonth) {
+    const isAfterPreviousMonth = dateLib.isAfter(
+      months[0].date,
+      previousMonths[0].date
+    );
+
+    const captionAnimationClass = isAfterPreviousMonth
+      ? classNames[Animation.caption_after_enter]
+      : classNames[Animation.caption_before_enter];
+
+    const weeksAnimationClass = isAfterPreviousMonth
+      ? classNames[Animation.weeks_after_enter]
+      : classNames[Animation.weeks_before_enter];
+
+    // get previous root element snapshot before updating the snapshot ref
+    const previousRootElSnapshot = previousRootElSnapshotRef.current;
+
+    // update snapshot for next effect trigger
+    const rootElSnapshot = rootElRef.current.cloneNode(true);
+    if (rootElSnapshot instanceof HTMLElement) {
+      // if this effect is triggered while animating, we need to clean up the new root snapshot
+      // to put it in the same state as when not animating, to correctly animate the next navigation
+      const currentMonthElsSnapshot = [
+        ...(rootElSnapshot.querySelectorAll(`[data-animated-month]`) ?? [])
+      ];
+      currentMonthElsSnapshot.forEach((currentMonthElSnapshot) => {
+        // remove the old month snapshots from the new root snapshot
+        const previousMonthElSnapshot = currentMonthElSnapshot.querySelector(
+          `[data-animated-month]`
+        );
+        if (
+          previousMonthElSnapshot &&
+          currentMonthElSnapshot.contains(previousMonthElSnapshot)
+        ) {
+          currentMonthElSnapshot.removeChild(previousMonthElSnapshot);
+        }
+
+        // remove animation classes from the new month snapshots
+        const captionEl = currentMonthElSnapshot.querySelector(
+          `[data-animated-caption]`
+        );
+        if (captionEl && captionEl instanceof HTMLElement) {
+          captionEl.classList.remove(captionAnimationClass);
+        }
+
+        const weeksEl = currentMonthElSnapshot.querySelector(
+          `[data-animated-weeks]`
+        );
+        if (weeksEl && weeksEl instanceof HTMLElement) {
+          weeksEl.classList.remove(weeksAnimationClass);
+        }
+      });
+
+      previousRootElSnapshotRef.current = rootElSnapshot;
+    } else {
+      previousRootElSnapshotRef.current = null;
+    }
+
+    if (
+      animatingRef.current ||
+      isSameMonth ||
+      // skip animation if a day is focused because it can cause issues to the animation and is better for a11y
+      focused
+    ) {
       return;
     }
 
@@ -107,10 +134,13 @@ export function useAnimation(
       animatingRef.current = true;
       const cleanUpFunctions: (() => void)[] = [];
 
-      const isAfterPreviousMonth = dateLib.isAfter(
-        months[0].date,
-        previousMonths[0].date
-      );
+      // set isolation to isolate to isolate the stacking context during animation
+      rootElRef.current.style.isolation = "isolate";
+      // set z-index to 1 to ensure the nav is clickable over the other elements being animated
+      const navEl = rootElRef.current.querySelector(`[data-animated-nav]`);
+      if (navEl && navEl instanceof HTMLElement) {
+        navEl.style.zIndex = "1";
+      }
 
       currentMonthEls.forEach((currentMonthEl, index) => {
         const previousMonthEl = previousMonthEls[index];
@@ -120,14 +150,6 @@ export function useAnimation(
         }
 
         // animate new displayed month
-        const captionAnimationClass = isAfterPreviousMonth
-          ? classNames[Animation.caption_after_enter]
-          : classNames[Animation.caption_before_enter];
-
-        const weeksAnimationClass = isAfterPreviousMonth
-          ? classNames[Animation.weeks_after_enter]
-          : classNames[Animation.weeks_before_enter];
-
         currentMonthEl.style.position = "relative";
         currentMonthEl.style.overflow = "hidden";
         const captionEl = currentMonthEl.querySelector(
@@ -145,6 +167,14 @@ export function useAnimation(
 
         const cleanUp = () => {
           animatingRef.current = false;
+
+          if (rootElRef.current) {
+            rootElRef.current.style.isolation = "";
+          }
+          if (navEl && navEl instanceof HTMLElement) {
+            navEl.style.zIndex = "";
+          }
+
           if (captionEl && captionEl instanceof HTMLElement) {
             captionEl.classList.remove(captionAnimationClass);
           }
