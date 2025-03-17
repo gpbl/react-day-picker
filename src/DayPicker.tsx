7242d0e982b901c6b0eacad9f106eb7314c60550
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2025-03-17 13:33:41 +0100

    fix: add visually hidden text to announce month/year (#2716)
    Visually hidden text announces month and year changes to screen readers.

--- src/DayPicker.tsx
+++ src/DayPicker.tsx
@@ -362,7 +362,7 @@ export function DayPicker(props: DayPickerProps) {
                           value={dateLib.getMonth(calendarMonth.date)}
                         />
                       ) : (
-                        <span role="status" aria-live="polite">
+                        <span>
                           {formatMonthDropdown(calendarMonth.date, dateLib)}
                         </span>
                       )}
@@ -380,10 +380,32 @@ export function DayPicker(props: DayPickerProps) {
                           value={dateLib.getYear(calendarMonth.date)}
                         />
                       ) : (
-                        <span role="status" aria-live="polite">
+                        <span>
                           {formatYearDropdown(calendarMonth.date, dateLib)}
                         </span>
                       )}
+                      <span
+                        role="status"
+                        aria-live="polite"
+                        style={{
+                          border: 0,
+                          clip: "rect(0 0 0 0)",
+                          height: "1px",
+                          margin: "-1px",
+                          overflow: "hidden",
+                          padding: 0,
+                          position: "absolute",
+                          width: "1px",
+                          whiteSpace: "nowrap",
+                          wordWrap: "normal"
+                        }}
+                      >
+                        {formatCaption(
+                          calendarMonth.date,
+                          dateLib.options,
+                          dateLib
+                        )}
+                      </span>
                     </components.DropdownNav>
                   ) : (
                     <components.CaptionLabel
