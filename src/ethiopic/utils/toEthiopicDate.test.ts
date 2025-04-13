8ea5804795966dae645c464da5f2c2f3b9081189
gpbl <io@gpbl.dev>
2025-04-13 09:18:54 -0500

    Rewrite toEthiopicDate tests

--- src/ethiopic/utils/toEthiopicDate.test.ts
+++ src/ethiopic/utils/toEthiopicDate.test.ts
@@ -1,62 +1,67 @@
 import { toEthiopicDate } from "./toEthiopicDate";
 
 describe("toEthiopicDate", () => {
-  test("should convert a Gregorian date to an Ethiopic date correctly", () => {
-    const testCases = [
-      {
-        gregorian: new Date(2024, 2, 9), // March 9, 2024
-        ethiopic: { year: 2016, month: 6, day: 30 }
-      },
-      {
-        gregorian: new Date(2024, 0, 1), // January 1, 2024
-        ethiopic: { year: 2016, month: 4, day: 22 }
-      },
-      {
-        gregorian: new Date(2023, 8, 11), // September 11, 2023
-        ethiopic: { year: 2015, month: 13, day: 6 } // Ethiopian New Year
-      },
-      {
-        gregorian: new Date(2024, 8, 10), // September 10, 2024
-        ethiopic: { year: 2016, month: 13, day: 5 } // Ethiopian New Year
-      }
-    ];
-
-    testCases.forEach(({ gregorian, ethiopic }) => {
-      expect(toEthiopicDate(gregorian)).toEqual(ethiopic);
+  test("converts March 9, 2024 to Ethiopic date correctly", () => {
+    expect(toEthiopicDate(new Date(2024, 2, 9))).toEqual({
+      year: 2016,
+      month: 6,
+      day: 30
     });
   });
 
-  test("should handle leap years correctly", () => {
-    const testCases = [
-      {
-        gregorian: new Date(2024, 1, 29), // February 29, 2024 (Gregorian leap year)
-        ethiopic: { year: 2016, month: 6, day: 21 }
-      },
-      {
-        gregorian: new Date(2023, 8, 11), // August 29, 2024
-        ethiopic: { year: 2015, month: 13, day: 6 } // Pagume (Ethiopian 13th month)
-      }
-    ];
-
-    testCases.forEach(({ gregorian, ethiopic }) => {
-      expect(toEthiopicDate(gregorian)).toEqual(ethiopic);
+  test("converts January 1, 2024 to Ethiopic date correctly", () => {
+    expect(toEthiopicDate(new Date(2024, 0, 1))).toEqual({
+      year: 2016,
+      month: 4,
+      day: 22
     });
   });
 
-  test("should handle dates before the Ethiopic epoch correctly", () => {
-    const testCases = [
-      {
-        gregorian: new Date(1900, 0, 1), // January 1, 1900
-        ethiopic: { year: 1892, month: 4, day: 23 }
-      },
-      {
-        gregorian: new Date(1800, 5, 15), // June 15, 1800
-        ethiopic: { year: 1792, month: 10, day: 9 }
-      }
-    ];
+  test("converts Ethiopian New Year (September 11, 2023) to Ethiopic date correctly", () => {
+    expect(toEthiopicDate(new Date(2023, 8, 11))).toEqual({
+      year: 2015,
+      month: 13,
+      day: 6
+    });
+  });
 
-    testCases.forEach(({ gregorian, ethiopic }) => {
-      expect(toEthiopicDate(gregorian)).toEqual(ethiopic);
+  test("converts Ethiopian New Year (September 10, 2024) to Ethiopic date correctly", () => {
+    expect(toEthiopicDate(new Date(2024, 8, 10))).toEqual({
+      year: 2016,
+      month: 13,
+      day: 5
+    });
+  });
+
+  test("handles Gregorian leap year February 29, 2024 correctly", () => {
+    expect(toEthiopicDate(new Date(2024, 1, 29))).toEqual({
+      year: 2016,
+      month: 6,
+      day: 21
+    });
+  });
+
+  test("handles Pagume (Ethiopian 13th month) September 11, 2024 correctly", () => {
+    expect(toEthiopicDate(new Date(2024, 8, 11))).toEqual({
+      year: 2016,
+      month: 13,
+      day: 6
+    });
+  });
+
+  test("handles January 1, 1900 correctly", () => {
+    expect(toEthiopicDate(new Date(1900, 0, 1))).toEqual({
+      year: 1892,
+      month: 4,
+      day: 23
+    });
+  });
+
+  test("handles June 15, 1800 correctly", () => {
+    expect(toEthiopicDate(new Date(1800, 5, 15))).toEqual({
+      year: 1792,
+      month: 10,
+      day: 9
     });
   });
 });
