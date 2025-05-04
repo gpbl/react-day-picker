bca65880b8bb0d4825864ff392697eef41c6d816
gpbl <io@gpbl.dev>
2025-05-04 17:04:06 -0500

    Cleanup useAnimation test
    Signed-off-by: gpbl <io@gpbl.dev>

--- src/useAnimation.test.tsx
+++ src/useAnimation.test.tsx
@@ -133,7 +133,7 @@ describe("useAnimation", () => {
     });
 
     it("should clean up the exiting month after animation ends", async () => {
-      render(<DayPicker animate={true} navLayout="after" />);
+      render(<DayPicker animate={true} />);
 
       await user.click(nextButton());
 
