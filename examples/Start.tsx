dd8a94008772055cfbdec507d4cef461f396ea83
gpbl <io@gpbl.dev>
2025-07-01 13:40:31 +0200

    Revert "fix: single month even when numberOfMonths"
    This reverts commit 0167b1ea8df69abf01445c18a7b966a217d1cd58.
    
    Signed-off-by: gpbl <io@gpbl.dev>

--- examples/Start.tsx
+++ examples/Start.tsx
@@ -1,26 +1,19 @@
 import React, { useState } from "react";
 
-import { DateRange, DayPicker } from "react-day-picker";
+import { DayPicker } from "react-day-picker";
 
 export function Start() {
-  const [dates, setDates] = useState<DateRange | undefined>({
-    from: new Date(),
-    to: new Date()
-  });
-  const [monthFocused, setMonthFocused] = useState(dates?.from);
+  const [selected, setSelected] = useState<Date>();
 
   return (
     <DayPicker
-      endMonth={new Date()}
-      disabled={{ after: new Date() }}
-      min={2}
-      numberOfMonths={2}
-      pagedNavigation={false}
-      mode="range"
-      selected={dates}
-      onSelect={setDates}
-      month={monthFocused}
-      onMonthChange={(start) => setMonthFocused(start)}
+      animate
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={
+        selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
+      }
     />
   );
 }
