7ef49e64d49483a865acd11a7ba4f6f2e7ade221
Takeru Chuganji <chu@ganzy.jp>
2025-08-03 14:38:52 +0900

    Add a test

--- examples/Dropdown.test.tsx
+++ examples/Dropdown.test.tsx
@@ -1,3 +1,4 @@
+import { ja } from "date-fns/locale";
 import React from "react";
 
 import { grid, monthDropdown, yearDropdown } from "@/test/elements";
@@ -11,19 +12,29 @@ const today = new Date(2015, 6, 1);
 beforeAll(() => jest.setSystemTime(today));
 afterAll(() => jest.useRealTimers());
 
-beforeEach(() => {
-  render(<Dropdown />);
-});
-
 test("should display the month dropdown", () => {
+  render(<Dropdown />);
   expect(monthDropdown()).toBeInTheDocument();
 });
 
 test("should display the year dropdown", () => {
+  render(<Dropdown />);
   expect(yearDropdown()).toBeInTheDocument();
 });
 
+test("should display the month and year dropdowns in the correct order", () => {
+  const { rerender } = render(<Dropdown />);
+  const comparePosition = () =>
+    monthDropdown().compareDocumentPosition(yearDropdown());
+
+  expect(comparePosition()).toBe(Node.DOCUMENT_POSITION_FOLLOWING);
+
+  rerender(<Dropdown locale={ja} />);
+  expect(comparePosition()).toBe(Node.DOCUMENT_POSITION_PRECEDING);
+});
+
 test("should disable the months before startMonth", () => {
+  render(<Dropdown />);
   const disabledMonth = [
     "January",
     "February",
@@ -40,6 +51,7 @@ test("should disable the months before startMonth", () => {
 });
 
 test("should disable the months after endMonth", async () => {
+  render(<Dropdown />);
   await user.selectOptions(yearDropdown(), "2025");
   const disabledMonth = ["November", "December"];
   for (const month of disabledMonth) {
@@ -51,6 +63,7 @@ test("should disable the months after endMonth", async () => {
 
 describe("when choosing a month", () => {
   test("should display the month", async () => {
+    render(<Dropdown />);
     const monthName = "December";
     await user.selectOptions(monthDropdown(), monthName);
     expect(grid()).toHaveAccessibleName(`${monthName} 2024`);
@@ -59,12 +72,14 @@ describe("when choosing a month", () => {
 
 describe("when choosing a year", () => {
   test("should display the year", async () => {
+    render(<Dropdown />);
     const year = "2025";
     await user.selectOptions(yearDropdown(), year);
     expect(grid()).toHaveAccessibleName(`July ${year}`);
   });
 
   test("should display the first available month when selecting a month before startMonth", async () => {
+    render(<Dropdown />);
     await user.selectOptions(yearDropdown(), "2025");
     await user.selectOptions(monthDropdown(), "January");
     await user.selectOptions(yearDropdown(), "2024");
@@ -72,6 +87,7 @@ describe("when choosing a year", () => {
   });
 
   test("should display the last available month when selecting a month after endMonth", async () => {
+    render(<Dropdown />);
     await user.selectOptions(monthDropdown(), "December");
     await user.selectOptions(yearDropdown(), "2025");
     expect(grid()).toHaveAccessibleName(`October 2025`);
@@ -80,6 +96,7 @@ describe("when choosing a year", () => {
 
 describe("when choosing a disabled month", () => {
   test("should display the first available month", async () => {
+    render(<Dropdown />);
     const monthName = "February";
     await user.selectOptions(monthDropdown(), monthName);
     expect(grid()).toHaveAccessibleName(`July 2024`);
--- examples/Dropdown.tsx
+++ examples/Dropdown.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-import { DayPicker } from "react-day-picker";
+import { DayPicker, type DayPickerProps } from "react-day-picker";
 
-export function Dropdown() {
+export function Dropdown(props: DayPickerProps) {
   return (
     <DayPicker
       defaultMonth={new Date(2024, 6)}
       captionLayout="dropdown"
       startMonth={new Date(2024, 6)}
       endMonth={new Date(2025, 9)}
+      {...props}
     />
   );
 }
