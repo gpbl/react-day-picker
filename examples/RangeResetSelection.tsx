f43f31e6047688037a10e409ecead13423c40b05
Rodolfo Gobbi De Angeli <rodolfodeangeli@gmail.com>
2025-08-14 14:40:30 +0200

    fix: select the same day in range mode when range is open and min prop is 0 (#2816)
    * examples: add RangeResetSelection
    
    * Allow selecting range with equal from and to dates
    
    * Add RangeResetSelection tests
    
    * fix lint

---
+++ examples/RangeResetSelection.tsx
@@ -0,0 +1,49 @@
+import { format } from "date-fns";
+import React, { useState } from "react";
+
+import {
+  type DateRange,
+  type DayEventHandler,
+  DayPicker,
+  type OnSelectHandler,
+} from "react-day-picker";
+
+export function RangeResetSelection() {
+  const [selected, setSelected] = useState<DateRange>();
+
+  // use onSelect event which properly handles valid range selection
+  // based on valid days in the calendar
+  const handleSelect: OnSelectHandler<DateRange | undefined> = (range) => {
+    // the other cases are handled by onDayClick handler
+    if (selected?.from && !selected.to) {
+      setSelected(range);
+    }
+  };
+
+  const handleDayClick: DayEventHandler<React.MouseEvent> = (date) => {
+    // handled by onSelect handler
+    if (selected?.from && !selected.to) {
+      return;
+    }
+    setSelected({ from: date });
+  };
+
+  return (
+    <DayPicker
+      mode="range"
+      selected={selected}
+      onSelect={handleSelect}
+      onDayClick={handleDayClick}
+      footer={
+        <div>
+          <p data-testid="from">
+            from: {selected?.from && format(selected?.from, "yyyy-MM-dd")}
+          </p>
+          <p data-testid="to">
+            to: {selected?.to && format(selected?.to, "yyyy-MM-dd")}
+          </p>
+        </div>
+      }
+    />
+  );
+}
