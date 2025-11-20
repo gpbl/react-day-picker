db9d97781cae05f1bd292ff360684273014d8c33
gpbl <io@gpbl.dev>
2025-11-20 21:12:55 +1100

    Lint file
    Signed-off-by: gpbl <io@gpbl.dev>

--- src/utils/toTimeZone.ts
+++ src/utils/toTimeZone.ts
@@ -2,8 +2,8 @@ import { TZDate } from "@date-fns/tz";
 
 /**
  * Convert a {@link Date} or {@link TZDate} instance to the given time zone.
- * Reuses the same instance when it is already a {@link TZDate} using the
- * target time zone to avoid extra allocations.
+ * Reuses the same instance when it is already a {@link TZDate} using the target
+ * time zone to avoid extra allocations.
  */
 export function toTimeZone(date: Date, timeZone: string): TZDate {
   if (date instanceof TZDate && date.timeZone === timeZone) {
