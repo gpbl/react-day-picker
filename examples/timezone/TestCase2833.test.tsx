a469c1a49d98bebaab8f046e7a79121ea166607b
gpbl <io@gpbl.dev>
2025-11-20 11:29:22 +0100

    Add test for current date to be different
    Signed-off-by: gpbl <io@gpbl.dev>

--- examples/timezone/TestCase2833.test.tsx
+++ examples/timezone/TestCase2833.test.tsx
@@ -8,16 +8,20 @@ test("test should run in Australia/Adelaide timezone", () => {
   );
 });
 
-test('the "now" paragraph should show the correct timezone', () => {
-  render(<TestCase2833 />);
-  expect(screen.getByTestId("now")).toHaveTextContent(
-    `Australian Central Daylight Time`,
+test("current date should be different than the date in Etc/GMT+12 timezone", () => {
+  const today = new Date();
+  const todayInTimezone = new Date(
+    today.toLocaleString("en-US", { timeZone: "Etc/GMT+12" }),
   );
-  expect(screen.getByTestId("timezone")).toHaveTextContent(`Etc/GMT+12`);
+  expect(todayInTimezone.getDate()).not.toBe(today.getDate());
 });
 
 test("today's date should not be disabled", () => {
   const { container } = render(<TestCase2833 />);
   const day = container.querySelector('[data-day="2025-11-19"]');
+  expect(screen.getByTestId("now")).toHaveTextContent(
+    `Australian Central Daylight Time`,
+  );
+  expect(screen.getByTestId("timezone")).toHaveTextContent(`Etc/GMT+12`);
   expect(day).not.toHaveAttribute("data-disabled", "true");
 });
