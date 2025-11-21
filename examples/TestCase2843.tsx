6a3b1f175d46195486cff2cac7f808d5325db8a7
gpbl <io@gpbl.dev>
2025-11-21 12:14:33 +0100

    Fix TestCase2843 today button interaction

---
+++ examples/TestCase2843.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import { type DateRange, DayPicker } from "react-day-picker";
+
+export function TestCase2843() {
+  const [selected, setSelected] = useState<DateRange>();
+
+  const disabled = [
+    ...(selected?.from ? [{ before: selected.from }] : []),
+    ...(selected?.from ? [selected.from] : []),
+  ];
+  
+  return (
+    <DayPicker
+      animate
+      mode="range"
+      selected={selected}
+      min={1}
+      disabled={disabled}
+      onSelect={(value, date) => {
+        setSelected((prevValue) => {
+          if (prevValue?.from && prevValue?.to) {
+            return {
+              from: date,
+              to: undefined,
+            };
+          }
+
+          return value;
+        });
+      }}
+    />
+  );
+}
