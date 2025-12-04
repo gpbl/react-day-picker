c3db6264651f848db4e78952b3aac3ae59f37a52
Giampaolo Bellavite <io@gpbl.dev>
2025-12-04 09:01:48 +0100

    fix: match Date[] modifiers by day (#2865)

---
+++ examples/TestCase2864.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { DayPicker } from "react-day-picker";
+
+/**
+ * Reproduction for Date[] matchers inside `disabled`.
+ * When passing `[dates]` as a matcher, the dates should be disabled.
+ */
+export function TestCase2864() {
+  const holidays = React.useMemo(
+    () => [new Date(2025, 0, 10), new Date(2025, 0, 20)],
+    [],
+  );
+
+  return (
+    <div>
+      <p>
+        January 10 and 20, 2025 should be disabled when the Date[] matcher is
+        passed directly to `disabled`.
+      </p>
+      <DayPicker month={new Date(2025, 0, 1)} disabled={[holidays]} />
+    </div>
+  );
+}
