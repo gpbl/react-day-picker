4988ad51512aeed96e3fb6cf93c01abbb8444564
Giampaolo Bellavite <io@gpbl.dev>
2025-12-07 22:47:23 +0100

    docs: add responsive markdown tables (#2875)
    * docs: add responsive markdown tables
    
    * Lint files
    
    Signed-off-by: gpbl <io@gpbl.dev>
    
    ---------
    
    Signed-off-by: gpbl <io@gpbl.dev>

---
+++ website/src/theme/MDXComponents.tsx
@@ -0,0 +1,29 @@
+// Import the original mapper
+import MDXComponents from "@theme-original/MDXComponents";
+import type { ComponentProps, ComponentType } from "react";
+import * as Examples from "react-day-picker/examples";
+
+import * as ExamplesV8 from "../../examples-v8";
+import { BrowserWindow } from "../components/BrowserWindow";
+
+type TableComponent = ComponentType<ComponentProps<"table">>;
+
+const Table: TableComponent =
+  (MDXComponents as { table?: TableComponent }).table ??
+  ((props) => <table {...props} />);
+
+function ResponsiveTable(props: ComponentProps<typeof Table>) {
+  return (
+    <div className="table-scroll" role="presentation">
+      <Table {...props} />
+    </div>
+  );
+}
+
+export default {
+  ...MDXComponents,
+  table: ResponsiveTable,
+  BrowserWindow,
+  Examples,
+  ExamplesV8,
+};
