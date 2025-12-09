d8b821be6867a9820993ae57ff96f6775357f2b7
gpbl <io@gpbl.dev>
2025-12-09 10:34:24 +0100

    Update test description
    Signed-off-by: gpbl <io@gpbl.dev>

--- examples/AsiaSaigonTimezone.test.tsx
+++ examples/AsiaSaigonTimezone.test.tsx
@@ -7,7 +7,7 @@ beforeEach(() => {
   render(<AsiaSaigonTimezone />);
 });
 
-test.skip('should have the "id" attribute', () => {
+test.skip("the first row should display 7 days", () => {
   expect(
     screen.getAllByRole("row")[0].querySelectorAll("[role='gridcell']"),
   ).toHaveLength(7);
