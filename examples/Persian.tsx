29afd86f0ca7e9e83a6c56bd21193778a900becb
Reza <rezamahmudy1@gmail.com>
2026-02-10 01:12:17 +0330

    fix: format Persian example to resolve lint error

--- examples/Persian.tsx
+++ examples/Persian.tsx
@@ -3,7 +3,5 @@ import React from "react";
 import { DayPicker } from "react-day-picker/persian";
 
 export function Persian() {
-  return (
-    <DayPicker mode="single" />
-  );
+  return <DayPicker mode="single" />;
 }
